b3450774b8a901032b1b97af14f40abc239245e8
Bruce Wayne <explorer436@tutanota.com>
2020-02-11 06:43:40 -0500

    updates to sorting class

--- MyJavaClasses/src/sorting/Insertion.java
+++ MyJavaClasses/src/sorting/Insertion.java
@@ -1,7 +1,11 @@
 package sorting;
 
 /*
- * Example:   The following table shows the sorting steps for sorting the sequence 5 7 0 3 4 2 6 1. On the left side, in green, is the already sorted part of the sequence. On the far right is the number of positions in parentheses around which the inserted element has moved to the left.
+ * Example:   
+ * The following table shows the sorting steps for sorting the sequence 5 7 0 3 4 2 6 1.
+ * On the left side, in green, is the already sorted part of the sequence. 
+ * On the far right is the number of positions in parentheses around which 
+ * the inserted element has moved to the left.
 
 5	7	0	3	4	2	6	1	 	(0)
 5	7	0	3	4	2	6	1	 	(0)
@@ -19,21 +23,27 @@ public class Insertion
 	public static void sort(Comparable[] a)
 	{
 		int length = a.length;
+		
 		for (int i = 1; i < length; i++)
 		{
+			System.out.println("\t i loop - i = " + i);
 			for (int j = i; j > 0; j--)
 			{
-				System.out.print("a[j - 1] : " + a[j - 1] + ", a[j] : " + a[j]);
-				if (a[j - 1].compareTo(a[j]) > 0)
+				boolean isTheFirstGreaterThanTheSecond = a[j - 1].compareTo(a[j]) > 0;
+				System.out.print("\t\t j loop - (j=" + j + ") - a[" + (j - 1) + "]=" + a[j - 1] + ", a[" + j + "] : " + a[j] + ", isTheFirstGreaterThanTheSecond : " + isTheFirstGreaterThanTheSecond);
+				if (isTheFirstGreaterThanTheSecond)
 				{
 					System.out.println(", need to exchange");
 					exch(a, j - 1, j);
 				}
 				else
 				{
-					System.out.println(", else");
+					// System.out.println(", do nothing.");
+					// break out of both the loops.
 					break;
 				}
+				System.out.println("\t\t end of j for loop.");
+				printArray(a);
 			}
 		}
 	}
@@ -59,11 +69,19 @@ public class Insertion
 
 		Integer[] intArray = new Integer[] { 5, 7, 0, 3, 4, 2, 6, 1 };
 
+		System.out.println("--------before sorting-----------");
+		printArray(intArray);
 		sort(intArray);
+		System.out.println("---------printing the sorted list------------");
+		printArray(intArray);
+		
+	}
+	
+	private static void printArray(Comparable[] intArray)
+	{
 		for (int i = 0; i < intArray.length; i++)
 		{
-			System.out.println(intArray[i] + " ");
+			System.out.print(intArray[i] + "\t");
 		}
-		System.out.println();
 	}
 }
