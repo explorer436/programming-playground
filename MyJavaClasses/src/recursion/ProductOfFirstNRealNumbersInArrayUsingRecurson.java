114e97d8fae3cf1751f2826a2c07ed14a307eac2
explorer436 <harshavardhane@gmail.com>
2020-02-28 14:59:52 -0500

    ProductOfFirstNRealNumbersInArrayUsingRecurson

--- MyJavaClasses/src/recursion/ProductOfFirstNRealNumbersInArrayUsingRecurson.java
+++ MyJavaClasses/src/recursion/ProductOfFirstNRealNumbersInArrayUsingRecurson.java
@@ -1,7 +1,42 @@
 package recursion;
 
-// Write a recursive function that computes and 
-// returns the product of the ﬁrs n ≥ 1 real numbers in an array.
-public class ProductOfFirstNRealNumbersInArrayUsingRecurson {
+import java.util.Arrays;
 
+/**
+ * 
+ * Write a recursive function that computes and 
+ * returns the product of the ﬁrst n ≥ 1 real numbers in an array.
+ *
+ */
+public class ProductOfFirstNRealNumbersInArrayUsingRecurson {
+	public static void main(String[] args) {
+
+        Integer[] anArray = new Integer[]{5, 2, 10, 1, 9, 3, 8, 4, 6, 7, 0};
+        System.out.println("productOfElements of anArray : " + ProductOfFirstNRealNumbersInArrayUsingRecurson.productOfElements(3, anArray));
+        
+        anArray = new Integer[]{5, 2, 10, 1, 9, 3, 8, 4, 6, 7, 0};
+        System.out.println("productOfElements of anArray : " + ProductOfFirstNRealNumbersInArrayUsingRecurson.productOfElements(1, anArray));
+        
+        anArray = new Integer[]{5, 2};
+        System.out.println("productOfElements of anArray : " + ProductOfFirstNRealNumbersInArrayUsingRecurson.productOfElements(3, anArray));
+    }
+
+    public static int productOfElements(int limit, Integer[] anArray) {
+        
+        int result = 0;
+
+        if (null != anArray && anArray.length != 0 && limit < anArray.length)
+        {
+        	if (limit == 1)
+            {
+                result = anArray[0];
+            }
+        	else if (limit <= anArray.length)
+            {
+        		result = anArray[0] * productOfElements(limit -1 , Arrays.copyOfRange(anArray, 1, anArray.length));
+            }
+        }
+
+        return result;
+    }
 }
