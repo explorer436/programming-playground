64b092ce76e40d57f68bbc5d23ae851d2d50f849
explorer436 <harshavardhane@gmail.com>
2020-03-13 14:56:17 -0400

    Update BinarySearch.java

--- MyJavaClasses/src/search/BinarySearch.java
+++ MyJavaClasses/src/search/BinarySearch.java
@@ -34,7 +34,36 @@ int binarySearch(const int anArray[], int ﬁrst, int last, int target)
 // end binarySearch
 
  */
+
 public class BinarySearch {
 
+	public static void main(String[] args) {
+		int[] anArray = new int[] {1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println("position of 4 is : " + BinarySearch.position(4, anArray));
+		System.out.println("position of 10 is : " + BinarySearch.position(10, anArray));
+		System.out.println("position of 15 is : " + BinarySearch.position(15, anArray));
+
+	}
 	
+	public static int position(int key, int[] a)
+	{
+		int lo = 0;		
+		int hi = a.length - 1;
+		while (lo <= hi)
+		{
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid]) {
+				hi = mid - 1;
+			}
+			else if (key > a[mid]) {
+				lo = mid + 1;
+			}
+			else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
 }
