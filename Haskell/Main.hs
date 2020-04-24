99460b615fdc9a7536b988106a5dfcf36f50c17e
explorer436 <harshavardhane@gmail.com>
2020-04-23 21:41:16 -0400

    Update Main.hs

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -15,3 +15,24 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 10
 then x
 else x*2
+
+l1 = [1,2,3,4]
+l2 = [9,10,11,12]
+puttingTwoListsTogether l1 l2 = l1 ++ l2
+
+printHelloWorld = "hello" ++ " " ++ "world!"
+
+printWoot = ['w','o'] ++ ['o','t']
+
+-- When you put together two lists (even if you append a singleton list to a list, for instance: [1,2,3] ++ [4]), 
+-- internally, Haskell has to walk through the whole list on the left side of ++. 
+-- That's not a problem when dealing with lists that aren't too big. 
+-- But putting something at the end of a list that's fifty million entries long is going to take a while. 
+-- However, putting something at the beginning of a list using the : operator (also called the cons operator) is instantaneous. 
+
+consOperatorFirstSample = 'A':" small cat"
+consOperatorSecondSample = 5:[1,2,3,4,5]
+
+-- The difference between ++ and the cons operator is, ++ operates on lists and cons operator operates on a single number (or character) and a list of numbers (or characters).
+
+getElementOutOfAListByIndex = "Steve Buschmi" !! 6
