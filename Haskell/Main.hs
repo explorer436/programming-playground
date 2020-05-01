dafef2fc19d27855e0ab85a91543f08d901ff32c
explorer436 <harshavardhane@gmail.com>
2020-04-30 22:21:56 -0400

    Update Main.hs

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,3 +1,6 @@
+-- Getting help : Enter :? at the ghci prompt
+
+-- exit or quit out of ghci : Ctrl D
 
 --  Open terminal and type ghci to launch ghci to go into GHCI!
 --  The prompt here is Prelude> and that will work for minor functions.
@@ -297,3 +300,40 @@ tupleRigtTriangleWithPerimeter24 = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [
 -- Explicit types are always denoted with the first letter in capital case.
 -- Unlike lists, each tuple length has its own type. So the expression of (True, 'a') has a type of (Bool, Char), 
 -- whereas an expression such as ('a','b','c') would have the type of (Char, Char, Char).
+
+-- ghci has a command, :set, that lets us change a few of its default behaviours. 
+-- We can tell it to print more type information as follows
+-- :set +t
+
+-- That it variable is a handy ghci shortcut. It lets us use the result of the expression we just evaluated in a new expression.
+-- ghci> "foo"
+-- "foo"
+-- it :: [Char]
+-- ghci> it ++ "bar"
+-- "foobar"
+-- it :: [Char]
+
+
+-- When evaluating an expression, ghci won't change the value of it if the evaluation fails. 
+-- This lets you write potentially bogus expressions with something of a safety net.
+
+-- ghci> it
+-- "foobar"
+-- it :: [Char]
+-- ghci> it ++ 3
+-- 
+-- <interactive>:1:6:
+--     No instance for (Num [Char])
+--       arising from the literal `3' at <interactive>:1:6
+--     Possible fix: add an instance declaration for (Num [Char])
+--     In the second argument of `(++)', namely `3'
+--     In the expression: it ++ 3
+--     In the definition of `it': it = it ++ 3
+-- ghci> it
+-- "foobar"
+-- it :: [Char]
+-- ghci> it ++ "baz"
+-- "foobarbaz"
+-- it :: [Char]
+-- 
+-- When we couple it with liberal use of the arrow keys to recall and edit the last expression we typed, we gain a decent way to experiment interactively: the cost of mistakes is very low.
