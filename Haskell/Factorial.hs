31c52ae6322f59a383286ff2b8969266befd372f
explorer436 <harshavardhane@gmail.com>
2020-05-01 16:41:54 -0400

    Update Factorial.hs

--- Haskell/Factorial.hs
+++ Haskell/Factorial.hs
@@ -6,3 +6,19 @@ fac 0 = 1
 fac n = n * fac (n-1)
 
 -- main = print (fac 42)
+factorialOfFive = fac 5
+
+
+customFactorialFunctionUsingRecursion :: (Integral a) => a -> a  
+customFactorialFunctionUsingRecursion 0 = 1  
+customFactorialFunctionUsingRecursion n = n * customFactorialFunctionUsingRecursion (n - 1) 
+
+
+customFactorialFunctionUsingRecursionTest1 = customFactorialFunctionUsingRecursion 3  
+
+
+
+customFactorialFunctionWithoutUsingRecursion :: (Integral a) => a -> a  
+customFactorialFunctionWithoutUsingRecursion n = product [1..n]  
+
+customFactorialFunctionWithoutUsingRecursionTest1 = customFactorialFunctionWithoutUsingRecursion 4  
