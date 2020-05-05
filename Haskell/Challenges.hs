4ef657ecd85bec6d2dae7b6413ea3c95306a1b7d
explorer436 <harshavardhane@gmail.com>
2020-05-05 17:50:54 -0400

    Update Challenges.hs

--- Haskell/Challenges.hs
+++ Haskell/Challenges.hs
@@ -1,3 +1,4 @@
+import Data.Char
 
 -- The problem we need to solve here is : read two numbers from standard input and print their sum on standard output.
 
@@ -128,15 +129,42 @@ Note: |x| is the absolute value of x-
 -}
 
 ------------------------------------------------------------
--- checkIfInputIsPalindrome :: [a] -> Boolean  
-checkIfInputIsPalindrome [] = False
-checkIfInputIsPalindrome [x] = True
-checkIfInputIsPalindrome xs 
+-- notice the differences in signature between caseSensitivePalindrome and caseInsensitivePalindrome.
+-- caseInsensitivePalindrome works only with strings.
+-- caseSensitivePalindddrom works with any TypeClass that satisfies Eq.
+caseSensitivePalindrome :: Eq t => [t] -> Bool
+caseSensitivePalindrome [] = False
+caseSensitivePalindrome [x] = True
+caseSensitivePalindrome xs 
     | xs == reverse xs = True
     | otherwise        = False
-checkIfInputIsPalindromeTest1 = checkIfInputIsPalindrome ""
-checkIfInputIsPalindromeTest2 = checkIfInputIsPalindrome "abc"
-checkIfInputIsPalindromeTest3 = checkIfInputIsPalindrome "deleveled"
-checkIfInputIsPalindromeTest4 = checkIfInputIsPalindrome "Deleveled"
--- TODO how to convert strings to lower case in Haskell?
+caseSensitivePalindromeTest1 = caseSensitivePalindrome ""
+caseSensitivePalindromeTest2 = caseSensitivePalindrome "a"
+caseSensitivePalindromeTest3 = caseSensitivePalindrome "abc"
+caseSensitivePalindromeTest4 = caseSensitivePalindrome "deleveled"
+caseSensitivePalindromeTest5 = caseSensitivePalindrome "Deleveled"
+
+------------------------------------------------------------
+-- notice the differences in signature between caseSensitivePalindrome and caseInsensitivePalindrome.
+-- caseInsensitivePalindrome works only with strings.
+-- caseSensitivePalindddrom works with any TypeClass that satisfies Eq.
+caseInsensitivePalindrome :: [Char] -> Bool
+caseInsensitivePalindrome [] = False
+caseInsensitivePalindrome [x] = True
+caseInsensitivePalindrome xs 
+    | lowerCasedString == reverse lowerCasedString = True
+    | otherwise                                      = False
+    where lowerCasedString = [ toLower c | c <- xs ]
+caseInsensitivePalindromeTest1 = caseInsensitivePalindrome ""
+caseInsensitivePalindromeTest2 = caseInsensitivePalindrome "a"
+caseInsensitivePalindromeTest3 = caseInsensitivePalindrome "abc"
+caseInsensitivePalindromeTest4 = caseInsensitivePalindrome "deleveled"
+caseInsensitivePalindromeTest5 = caseInsensitivePalindrome "Deleveled"
+
+------------------------------------------------------------
+-- How to convert a word to lower case in Haskell?
+lowerString :: [Char] -> [Char]
+lowerString str = [ toLower c | c <- str]
+lowerStringTest1 = lowerString "Abc"
+
 ------------------------------------------------------------
