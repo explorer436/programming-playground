9dcc47826f1818166132937b49047bde11ab10be
explorer436 <harshavardhane@gmail.com>
2020-05-08 09:11:43 -0400

    Update OddNumberOfAnArray.java

--- MyJavaClasses/src/codility/arrays/OddNumberOfAnArray.java
+++ MyJavaClasses/src/codility/arrays/OddNumberOfAnArray.java
@@ -50,16 +50,69 @@ public class OddNumberOfAnArray
 	{
 		System.out.println("starting test class");
 
-		OddNumberOfAnArray sol = new OddNumberOfAnArray();
+		OddNumberOfAnArray classUnderTest = new OddNumberOfAnArray();
 
-		int finalResult = sol.solution(new int[] { 9, 3, 9, 3, 9, 7, 9 });
-		System.out.println("finalResult : " + finalResult);
+		System.out.println("result for input { 9, 3, 9, 3, 9, 7, 9 }) : " + classUnderTest.solution(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
 
-		finalResult = sol.solution(new int[] { 1 });
-		System.out.println("finalResult : " + finalResult);
+		System.out.println("result for input { 1 } : " + classUnderTest.solution(new int[] { 1 }));
+		
+		System.out.println("result for input { } : " + classUnderTest.solution(new int[] { }));
 
 	}
 
+	/*
+	 *  "^" - bitwise exclusive OR
+	 *      - bitwise XOR
+	 *  It tells whether there is an odd number of 1 bits 
+	 *  (A XOR B XOR C XOR D XOR E} is true iff an odd number of the variables are true).    
+	 */
+	
+	/*
+	 *  Bits for 0 - 0000
+	 *  Bits for 9 - 1001
+	 *  Bits for 3 - 0011
+	 *  Bits for 7 - 0111
+	 */
+	
+	/*
+	 *  XOR on 0 and { 9, 3, 9, 3, 9, 7, 9 }
+	 *  
+	 *  XOR on 0 and 9 - 0000
+	 *                   1001
+	 *                   ----
+	 *                   1001
+	 *                   
+	 *  XOR on 1001 and 3 - 1001
+	 *                      0011
+	 *                      ----
+	 *                      1010 
+	 *                      
+	 *  XOR on 1010 and 9 - 1010
+	 *                      1001
+	 *                      ----
+	 *                      0011 
+	 *                      
+	 *  XOR on 0011 and 3 - 0011
+	 *                      0011
+	 *                      ----
+	 *                      0000
+	 *                      
+	 *  XOR on 0000 and 9 - 0000
+	 *                      1001
+	 *                      ----
+	 *                      1001  
+	 *                      
+	 *  XOR on 1001 and 7 - 1001
+	 *                      0111
+	 *                      ----
+	 *                      1110
+	 *                      
+	 *  XOR on 0000 and 9 - 1110
+	 *                      1001
+	 *                      ----
+	 *                      0111   - this evaluates to 7                                                           
+	 */
+	
 	public int solution(int[] A)
 	{
 		int result = 0;
