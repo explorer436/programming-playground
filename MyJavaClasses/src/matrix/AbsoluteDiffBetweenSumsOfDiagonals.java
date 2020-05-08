132f544bb79ea751a51cdacd26a5dd5be5d63761
explorer436 <harshavardhane@gmail.com>
2020-05-08 11:43:26 -0400

    Update AbsoluteDiffBetweenSumsOfDiagonals.java

--- MyJavaClasses/src/matrix/AbsoluteDiffBetweenSumsOfDiagonals.java
+++ MyJavaClasses/src/matrix/AbsoluteDiffBetweenSumsOfDiagonals.java
@@ -1,5 +1,7 @@
 package matrix;
 
+import java.util.Arrays;
+
 /**
  * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
@@ -67,9 +69,77 @@ Note: |x| is the absolute value of x-
  */
 public class AbsoluteDiffBetweenSumsOfDiagonals {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		int [][]a = { 
+				{ 11, 2, 4 }, 
+                { 4, 5, 6 },
+                { 10, 8, -12 } 
+		}; 
+		
+		AbsoluteDiffBetweenSumsOfDiagonals classUnderTest = new AbsoluteDiffBetweenSumsOfDiagonals();
+		
+		System.out.println("absoluteDiffBetweenSumsOfDiagonals for arrary " + Arrays.deepToString(a) + " is " + classUnderTest.absoluteDiffBetweenSumsOfDiagonals(a));
+		
+		System.out.println("sumOfElementsOfPrimaryDiagonal for array " + Arrays.deepToString(a) + " is " + classUnderTest.sumOfElementsOfPrimaryDiagonal(a));
+		
+		System.out.println("sumOfElementsOfSecondaryDiagonal " + Arrays.deepToString(a) + " is " + classUnderTest.sumOfElementsOfSecondaryDiagonal(a));
+		
+		int alternateSolutionForA = Math.abs(classUnderTest.sumOfElementsOfPrimaryDiagonal(a) - classUnderTest.sumOfElementsOfSecondaryDiagonal(a));
+		System.out.println("alternateSolution for array " + Arrays.deepToString(a) + " is " + alternateSolutionForA);
+		
+		System.out.println();
+		
+		int [][] b = { 
+				{ 1, 2, 3 }, 
+                { 4, 5, 6 },
+                { 9, 8, 9 } 
+		}; 
+		
+		System.out.println("absoluteDiffBetweenSumsOfDiagonals for arrary " + Arrays.deepToString(b) + " is " + classUnderTest.absoluteDiffBetweenSumsOfDiagonals(b));
+		
+		System.out.println("sumOfElementsOfPrimaryDiagonal for array " + Arrays.deepToString(b) + " is " + classUnderTest.sumOfElementsOfPrimaryDiagonal(b));
+		
+		System.out.println("sumOfElementsOfSecondaryDiagonal for array " + Arrays.deepToString(b) + " is " + classUnderTest.sumOfElementsOfSecondaryDiagonal(b));
+		
+		int alternateSolutionForB = Math.abs(classUnderTest.sumOfElementsOfPrimaryDiagonal(b) - classUnderTest.sumOfElementsOfSecondaryDiagonal(b));
+		System.out.println("alternateSolution for array " + Arrays.deepToString(b) + " is " + alternateSolutionForB);
 	}
-
+	
+	private int absoluteDiffBetweenSumsOfDiagonals(int[][] array)
+	{
+		int result = 0;
+		
+		for (int i = 0; i < array.length; i++)
+		{
+			int differenceBetweenElementsOfPrimaryAndSecondaryDiagonals = array[i][i] - array[i][(array.length -1) - i];
+			result = result + differenceBetweenElementsOfPrimaryAndSecondaryDiagonals;
+		}
+		
+		return Math.abs(result);
+	}
+	
+	private int sumOfElementsOfPrimaryDiagonal(int[][] array)
+	{
+		int result = 0;
+		for (int i = 0; i < array.length; i++)
+		{
+			result = result + array[i][i];
+		}
+		return result;
+	}
+	
+	private int sumOfElementsOfSecondaryDiagonal(int[][] array)
+	{
+		int result = 0;
+		for (int i = 0; i < array.length; i++)
+		{
+			result = result + array[i][(array.length - 1) - i];
+		}
+		return result;
+	}
+	
+	// TODO 
+	// There are a lot of helpful methods for matrices here : https://introcs.cs.princeton.edu/java/95linear/Matrix.java.html
+	// Take a look at them.
 }
