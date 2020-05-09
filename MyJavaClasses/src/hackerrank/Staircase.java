492e5712e2193a9cde86fac02ea42f3b7d822572
explorer436 <harshavardhane@gmail.com>
2020-05-09 15:36:34 -0400

    Update Staircase.java

--- MyJavaClasses/src/hackerrank/Staircase.java
+++ MyJavaClasses/src/hackerrank/Staircase.java
@@ -47,6 +47,27 @@ The staircase is right-aligned, composed of # symbols and spaces, and has a heig
  */
 
 public class Staircase {
-    // TODO
+
+	// Regex
+	
+	public static void main(String[] args) {
+		staircase(4);
+		staircase(6);
+	}
+	
+	static void staircase(int n) {
+		
+		String whitespace = " ";
+		String hashSymbol = "#";
+		
+		for (int i = 1; i <= n; i++)
+		{			
+			int numberOfWhiteSpaces = n - i;
+			System.out.print(new String(new char[numberOfWhiteSpaces]).replace("\0", whitespace));
+			
+			int numberOfHashes = i;
+			System.out.println(new String(new char[numberOfHashes]).replace("\0", hashSymbol));
+		}
+	}
     
 }
