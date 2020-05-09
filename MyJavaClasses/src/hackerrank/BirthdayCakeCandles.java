affa8508b2b50671db47bbbeb9316913df677924
explorer436 <harshavardhane@gmail.com>
2020-05-09 15:37:00 -0400

    Update BirthdayCakeCandles.java

--- MyJavaClasses/src/hackerrank/BirthdayCakeCandles.java
+++ MyJavaClasses/src/hackerrank/BirthdayCakeCandles.java
@@ -1,8 +1,14 @@
 package hackerrank;
 
+import java.util.Arrays;
+
 /**
- * You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
-For example, if your niece is turning 4 years old, and the cake will have candles of height 4, 4, 1, 3, she will be able to blow out 2 candles successfully, since the tallest candles are of height 4 and there are 2 such candles.
+ * You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. 
+ * When she blows out the candles, she’ll only be able to blow out the tallest ones. 
+ * Your task is to find out how many candles she can successfully blow out.
+For example, if your niece is turning 4 years old, and the cake will have candles of height 4, 4, 1, 3, 
+she will be able to blow out 2 candles successfully, 
+since the tallest candles are of height 4 and there are 2 such candles.
 
 Function Description:
 Complete the function birthdayCakeCandles in the editor below. It must return an integer representing the number of candles she can blow out.
@@ -21,14 +27,14 @@ Constraints:
 Output Format:
 Return the number of candles that can be blown out on a new line.
 
-Sample Input 0
+Sample Input: 
 4
 3 2 1 3
 
-Sample Output 0
+Sample Output: 
 2
 
-Explanation 0
+Explanation:
 We have one candle of height 1, one candle of height 2, and two candles of height 3. 
 Your niece only blows out the tallest candles, meaning the candles where height=3. 
 Because there are 2 such candles, we print 2 on a new line. 
@@ -36,6 +42,37 @@ Because there are 2 such candles, we print 2 on a new line.
  */
 
 public class BirthdayCakeCandles {
-    // TODO
+	
+	public static void main(String[] args) {
+		System.out.println("result for input " + Arrays.toString(new int[] { 3, 2, 1, 3 }) + " is " + birthdayCakeCandles(new int[] { 3, 2, 1, 3 }));
+		
+		System.out.println();
+		
+		System.out.println("result for input " + Arrays.toString(new int[] { 4, 4, 1, 3 }) + " is " + birthdayCakeCandles(new int[] { 4, 4, 1, 3 }));
+		
+		System.out.println();
+
+	}
+	
+	static int birthdayCakeCandles(int[] ar) {
+
+		Integer highestLengthCandle = ar[0];
+		Integer count = 0;
+		
+		for (int i = 0; i < ar.length; i++)
+		{
+			if (ar[i] > highestLengthCandle)
+			{
+				highestLengthCandle = ar[i];
+				count = 1;
+			}
+			else if (ar[i] == highestLengthCandle)
+			{
+				count = count + 1;
+			}
+		}
+		
+		return count;
+	}
     
 }
