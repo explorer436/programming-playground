b578f3e14847c7e0cbf8e240e0f0e2ab24126c51
explorer436 <harshavardhane@gmail.com>
2020-05-09 11:34:57 -0400

    Update MiniMaxSum.java

--- MyJavaClasses/src/hackerrank/MiniMaxSum.java
+++ MyJavaClasses/src/hackerrank/MiniMaxSum.java
@@ -1,5 +1,6 @@
 package hackerrank;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 /**
@@ -60,26 +61,49 @@ public class MiniMaxSum {
 		
 		System.out.println("result for input " + Arrays.toString(new int[] { -1, -2, 3, 4, 5 }) + " is ");
 		miniMaxSum(new int[] { -1, -2, 3, 4, 5 });
+		
+		System.out.println();
+		
+		System.out.println("result for input " + Arrays.toString(new int[] { 7, 69, 2, 211, 8974 }) + " is ");
+		miniMaxSum(new int[] { 7, 69, 2, 211, 8974 });
+		
+		System.out.println();
+		
+		System.out.println("result for input " + Arrays.toString(new int[] { 5, 5, 5, 5, 5 }) + " is ");
+		miniMaxSum(new int[] { 5, 5, 5, 5, 5 });
+		
+		System.out.println();
+		
+		System.out.println("result for input " + Arrays.toString(new int[] { 793810624, 895642170, 685903712, 623789054, 468592370 }) + " is ");
+		miniMaxSum(new int[] { 793810624, 895642170, 685903712, 623789054, 468592370 });
+		// Expected Output 		    2572095760 2999145560
 
 	}
 	
 	static void miniMaxSum(int[] ar) {
-		Integer totalSum = simpleArraySum(ar);
+		BigDecimal totalSum = simpleArraySum(ar);
 	
-		// initialize the values to the opposite extremes.
-		Integer maxSum = Integer.MIN_VALUE;
-		Integer minSum = Integer.MAX_VALUE;
+		// initialize the values to totalSum minus the first element in the list.
+		BigDecimal maxSum = totalSum.subtract(new BigDecimal(ar[0]));
+		BigDecimal minSum = totalSum.subtract(new BigDecimal(ar[0]));
         
         for (int i : ar)
         {
-        	if ((totalSum - i) > maxSum)
+        	// BigDecimalA.compareTo(BigDecimalB) : -1, 0, or 1 as BigDecimalB is numerically less than, equal to, or greater than BigDecimalA.
+        	
+        	BigDecimal totalSumMinusCurrentElement = totalSum.subtract(new BigDecimal(i));
+        	
+        	boolean isTotalSumMinusCurrentElementGreaterThanCurrentMaxSum = totalSumMinusCurrentElement.compareTo(maxSum) > 0 ? true : false;
+        	boolean isTotalSumMinusCurrentElementLesserThanCurrentMinSum = minSum.compareTo(totalSumMinusCurrentElement) > 0 ? true : false;
+        	       	
+        	if (isTotalSumMinusCurrentElementGreaterThanCurrentMaxSum)
         	{
-        		maxSum = (totalSum - i);
+        		maxSum = totalSumMinusCurrentElement;
         	}
         	
-        	if ((totalSum - i) < maxSum)
+        	if (isTotalSumMinusCurrentElementLesserThanCurrentMinSum)
         	{
-        		minSum = (totalSum - i);
+        		minSum = totalSumMinusCurrentElement;
         	}
         }
         
@@ -87,17 +111,25 @@ public class MiniMaxSum {
 
     }
 	
-	static Integer simpleArraySum(int[] ar) {
-		Integer result = 0;
+	static BigDecimal simpleArraySum(int[] ar) {
+		BigDecimal result = new BigDecimal(0);
         
         if (ar.length > 0)
         {
-        	for (int i : ar)
-            result = result + i;	
+        	for (int i : ar) {
+        		result = result.add(new BigDecimal(i));
+        	}
         }
         
         return result;
 
     }
+	
+	// LESSONSLEARNED
+	// Started out solving for this using integers. That blew up as the ranges of the numbers in the test cases kept growing bigger.
+	// When doing operations with BigInteger, do not try to convert the result back into int using BigInteger.intValue(). 
+	// Not sure what that is doing but it was giving negative values for large BigDecimal values.
+	// Just print the BigDecimal values on the console.
+	// This might turn into a problem in scenarios where an integer or a long value has to be returned from the method. 
     
 }
