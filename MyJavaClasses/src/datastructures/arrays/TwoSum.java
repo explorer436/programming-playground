20e4a619714d4737fbd875135f0db1f499f81943
explorer436 <harshavardhane@gmail.com>
2020-05-15 08:19:18 -0400

    Update TwoSum.java

--- MyJavaClasses/src/datastructures/arrays/TwoSum.java
+++ MyJavaClasses/src/datastructures/arrays/TwoSum.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import collections.MapUtils;
+
 /**
  * Two Sum
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -18,66 +20,64 @@ public class TwoSum
 
     public static void main(String[] args)
     {
+    	// num : [3, 2, 4], target : 6, Expected : [0, 1]
         int[] nums = new int[] { 2, 7, 11, 15 };
-
-        // num : [2, 7, 11, 15], target : 9, Expected : [0,1]
-        Arrays.stream(new TwoSum().twoSum(nums, 9)).forEach(e -> System.out.print(e + " "));
-        System.out.println("-------");
+        System.out.println("When input is " + Arrays.toString(nums)  + " and target is 9, output is " + Arrays.toString(new TwoSum().twoSum(nums, 9)));
+        
+        System.out.println();
 
         // num : [3, 2, 4], target : 6, Expected : [1,2]
         nums = new int[] { 3, 2, 4 };
-        Arrays.stream(new TwoSum().twoSum(nums, 6)).forEach(e -> System.out.print(e + " "));
-        System.out.println("-------");
+        System.out.println("When input is " + Arrays.toString(nums)  + " and target is 9, output is " + Arrays.toString(new TwoSum().twoSum(nums, 9)));
+        System.out.println("When input is " + Arrays.toString(nums)  + " and target is 6, output is " + Arrays.toString(new TwoSum().twoSum(nums, 6)));
+        
+        System.out.println();
 
         // num : [0, 4, 3, 0], target : 0, Expected : [0,3]
         nums = new int[] { 0, 4, 3, 0 };
-        Arrays.stream(new TwoSum().twoSum(nums, 0)).forEach(e -> System.out.print(e + " "));
-        System.out.println("-------");
-
+        System.out.println("When input is " + Arrays.toString(nums)  + " and target is 0, output is " + Arrays.toString(new TwoSum().twoSum(nums, 0)));
+        
+        System.out.println();
+        
         // num : [-1,-2,-3,-4,-5], target : -8, Expected : [2,4]
         nums = new int[] { -1, -2, -3, -4, -5 };
-        Arrays.stream(new TwoSum().twoSum(nums, -8)).forEach(e -> System.out.print(e + " "));
-        System.out.println("-------");
+        System.out.println("When input is " + Arrays.toString(nums)  + " and target is -8, output is " + Arrays.toString(new TwoSum().twoSum(nums, -8)));
     }
-
-    // Runtime: 253 ms, faster than 5.07% of Java online submissions for Two Sum.
-    // Memory Usage: 38.2 MB, less than 90.03% of Java online submissions for Two Sum.
-    // public int[] twoSum(int[] nums, int target)
-    // {
-    // int result[] = new int[2];
-
-    // for (int j = 0; j < nums.length; j++)
-    // {
-    // System.out.println("j : " + j);
-    // System.out.println("nums[j] : " + nums[j]);
-
-    // System.out.println("inside the if loop");
-    // int remaining = target - nums[j];
-    // System.out.println("remaining : " + remaining);
-    // int indexOfRemaining = IntStream.range(0, nums.length)
-    // .filter(i -> remaining == nums[i])
-    // .findFirst()
-    // .orElse(-1);
-    // System.out.println("indexOfRemaining : " + indexOfRemaining);
-    // if (-1 != indexOfRemaining && j != indexOfRemaining)
-    // {
-    // result[0] = j;
-    // result[1] = indexOfRemaining;
-    // break;
-    // }
-    // }
-
-    // return result;
-
-    // }
-
+    
     public int[] twoSum(int[] nums, int target)
     {
         Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+        	int complement = target - nums[i];
+        	
+        	if (map.containsKey(complement) && map.get(complement) != i)
+            {
+                return new int[] { map.get(complement), i };
+            }
+            else
+            {
+            	map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * The difference between twoSum and twoSum2 is that, with twoSum2, we are running through all the elements of the input array all the time.
+     * And that may not be necessary.
+     * 
+     */
+    public int[] twoSum2(int[] nums, int target)
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < nums.length; i++)
         {
             map.put(nums[i], i);
         }
+        
         for (int i = 0; i < nums.length; i++)
         {
             int complement = target - nums[i];
@@ -86,7 +86,7 @@ public class TwoSum
                 return new int[] { i, map.get(complement) };
             }
         }
-        // throw new IllegalArgumentException("No two sum solution");
+        
         return null;
     }
 
