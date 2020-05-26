8163d3bce7e374c10da9a9cd7fc3eb8caf5a3870
explorer436 <harshavardhane@gmail.com>
2020-05-26 16:07:30 -0400

    Update PassingCars.java

--- MyJavaClasses/src/codility/prefixsums/PassingCars.java
+++ MyJavaClasses/src/codility/prefixsums/PassingCars.java
@@ -1,20 +1,21 @@
 package codility.prefixsums;
 
 /**
+ * 
  * A non-empty zero-indexed array A consisting of N integers is given. The
  * consecutive elements of array A represent consecutive cars on a road.
  * 
  * Array A contains only 0s and/or 1s:
  * 
  * 0 represents a car traveling east, 1 represents a car traveling west. The
- * goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P
+ * goal is to count passing cars. We say that a pair of cars (P, Q), where 0 â‰¤ P
  * < Q < N, is passing when P is traveling to the east and Q is traveling to the
  * west.
  * 
  * For example, consider array A such that:
  * 
- * A[0] = 0 A[1] = 1 A[2] = 0 A[3] = 1 A[4] = 1 We have five pairs of passing
- * cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+ * A[0] = 0 A[1] = 1 A[2] = 0 A[3] = 1 A[4] = 1 
+ * We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
  * 
  * Write a function:
  * 
@@ -23,13 +24,11 @@ package codility.prefixsums;
  * that, given a non-empty zero-indexed array A of N integers, returns the
  * number of pairs of passing cars.
  * 
- * The function should return −1 if the number of pairs of passing cars exceeds
- * 1,000,000,000.
+ * The function should return -1 if the number of pairs of passing cars exceeds 1,000,000,000.
  * 
  * For example, given:
  * 
- * A[0] = 0 A[1] = 1 A[2] = 0 A[3] = 1 A[4] = 1 the function should return 5, as
- * explained above.
+ * A[0] = 0 A[1] = 1 A[2] = 0 A[3] = 1 A[4] = 1 (A = [ 0, 1, 0, 1, 1]) the function should return 5, as explained above.
  * 
  * Assume that:
  * 
@@ -39,9 +38,11 @@ package codility.prefixsums;
  * expected worst-case time complexity is O(N); expected worst-case space
  * complexity is O(1), beyond input storage (not counting the storage required
  * for input arguments).
+ * 
  */
 public class PassingCars
 {
+	// A = [ 0, 1, 0, 1, 1]
 
 	public static void main(String[] args) throws Exception
 	{
@@ -63,6 +64,9 @@ public class PassingCars
 		System.out.println("all good");
 	}
 
+	/*
+	 * assuming 0 represents cars going in the right direction and 1 represents cars going in the left direction.
+	 */
 	public static int solution(int[] A)
 	{
 		int right_count = 0, passingCars = 0;
@@ -75,7 +79,8 @@ public class PassingCars
 			}
 			if (A[i] == 1)
 			{
-				// the car is going left
+				// the car is going left.
+				// all the cars going in the left direction will pass the cars that are going right.
 				passingCars = passingCars + right_count;
 			}
 			if (passingCars > 1000000000)
