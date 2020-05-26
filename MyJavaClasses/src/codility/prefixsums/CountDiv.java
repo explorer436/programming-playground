16d9e83d29aea0ba9c818dfb7eff1585098bc275
explorer436 <harshavardhane@gmail.com>
2020-05-26 16:07:54 -0400

    Update CountDiv.java

--- MyJavaClasses/src/codility/prefixsums/CountDiv.java
+++ MyJavaClasses/src/codility/prefixsums/CountDiv.java
@@ -8,7 +8,7 @@ package codility.prefixsums;
  * that, given three integers A, B and K, returns the number of integers within
  * the range [A..B] that are divisible by K, i.e.:
  * 
- * { i : A ≤ i ≤ B, i mod K = 0 }
+ * { i : A <= i <= B, i mod K = 0 }
  * 
  * For example, for A = 6, B = 11 and K = 2, your function should return 3,
  * because there are three numbers divisible by 2 within the range [6..11],
@@ -17,70 +17,68 @@ package codility.prefixsums;
  * Assume that:
  * 
  * A and B are integers within the range [0..2,000,000,000]; K is an integer
- * within the range [1..2,000,000,000]; A ≤ B. Complexity:
+ * within the range [1..2,000,000,000]; A <= B. 
  * 
+ * Complexity:
  * expected worst-case time complexity is O(1); expected worst-case space
  * complexity is O(1).
  */
 public class CountDiv
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		System.out.println("result : " + solution(6, 11, 2));
-
-		System.out.println("result : " + solution(6, 11, 3));
-
-		System.out.println("result : " + solution(11, 345, 17));// answer should be 20
+		int result = solution(6, 11, 2);
+		if (3 != result)
+		{
+			throw new Exception("wrong answer - expected " + 3 + " but received " + result);
+		}
+		
+		result = solution(6, 11, 3);
+		if (2 != result)
+		{
+			throw new Exception("wrong answer - expected " + 2 + " but received " + result);
+		}
+		
+		result = solution(6, 11, 3);
+		if (20 != result)
+		{
+			throw new Exception("wrong answer - expected " + 20 + " but received " + result);
+		}
 
 		/*
 		 * A = 11, B = 345, K = 17 WRONG ANSWER got 19 expected 20
 		 */
-
-		/*
-		 * System.out.println("result : " + solution(2, A3));
-		 * 
-		 * System.out.println("result : " + solution(3, A4));
-		 * 
-		 * System.out.println("result : " + solution(1, A5));
-		 * 
-		 * System.out.println("result : " + solution(2, A5));
-		 */
 	}
 
-	// how is this solution using prefix sums?
+	// How is this solution using prefix sums?
+	// Using the concept of prefix sums helps with the fact that (B/K) - ((A-1)/K) is not the same as (B - (A - 1))/K
 	public static int solution(int A, int B, int K)
 	{
 		// Explanation: Number of integers in the range [1 .. X] that divisible by K is
 		// X/K.
 		// So, within the range [A .. B], the result is B/K - (A - 1)/K
+		
+		// Note : This is not the same as (B - (A -1))/K
 
-		int b = B / K;
+		int bDividedByK = B / K;
 
-		int a;
+		int aDividedByK;
 		if (A > 0)
 		{
-			a = (A - 1) / K;
+			aDividedByK = (A - 1) / K;
 		}
 		else
 		{
-			a = 0;
+			aDividedByK = 0;
 		}
 
 		if (0 == A)
 		{
-			b = b + 1;
+			bDividedByK = bDividedByK + 1;
 		}
 
-		return b - a;
+		return bDividedByK - aDividedByK;
 	}
-
-	/*
-	 * public static int solution(int A, int B, int K) { int result = 0; int diff =
-	 * (B - A);
-	 * 
-	 * if(0 == diff) { if(0 == (A % K)) { return 1; } else { return 0; } } else {
-	 * if((0 == (A % K)) || (0 == (B % K))) { result = (diff/K) +1; } else { result
-	 * = (diff/K); } } return result; }
-	 */
+	
 }
