71f2e1aec4736cb3888ceb4802113221ec427a9b
explorer436 <harshavardhane@gmail.com>
2020-05-28 10:43:13 -0400

    Update MushroomPicker.java

--- MyJavaClasses/src/codility/prefixsums/MushroomPicker.java
+++ MyJavaClasses/src/codility/prefixsums/MushroomPicker.java
@@ -187,23 +187,5 @@ public class MushroomPicker
     	
     	return currentMaxMushroomsPickedUp;
     }
-    
-    private static int[] prefixSumsOfAnArray(int A[])
-    {
-    	int[] prefixSums = new int[A.length];
-    	
-    	if (A.length > 0)
-    	{
-    		prefixSums[0] = A[0];
-    		for (int index = 1; index < A.length; index++)
-        	{
-    			int previousP = prefixSums[index - 1];
-    			int currentA = A[index];
-        		prefixSums[index] = previousP + currentA;
-        	}
-    	}
-    	
-    	return prefixSums;
-    }
 
 }
