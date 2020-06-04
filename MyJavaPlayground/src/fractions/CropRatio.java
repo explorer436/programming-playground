817e62af96dff97a572ef30d3f869cb9c3def201
Bruce Wayne <explorer436@tutanota.com>
2020-06-03 22:01:52 -0400

    renaming the java project

---
+++ MyJavaPlayground/src/fractions/CropRatio.java
@@ -0,0 +1,55 @@
+package fractions;
+
+import java.util.HashMap;
+
+import utility.PrintUtils;
+
+/*
+ * Calculate the ratio of each crop - based on the values being added in the main method.
+ */
+public class CropRatio
+{
+    private int totalWeight;
+
+    private HashMap<String, Integer> crops = new HashMap<String, Integer>();
+
+    public void add(String name, int cropWeight)
+    {
+        Integer currentCropWeight = (Integer) crops.get(name);
+
+        if (currentCropWeight == null)
+        {
+            crops.put(name, (Integer) cropWeight);
+        }
+        else
+        {
+            currentCropWeight = currentCropWeight + cropWeight;
+            crops.put(name, (Integer) currentCropWeight);
+        }
+
+        PrintUtils.printMap(crops);
+
+        totalWeight = totalWeight + cropWeight;
+    }
+
+    public double proportion(String name)
+    {
+        // System.out.println(">>> proportion - name : " + name);
+        // System.out.println("crops.get(name) : " + (Integer) crops.get(name));
+        // System.out.println("totalWeight : " + totalWeight);
+        return ((double) crops.get(name)) / totalWeight;
+    }
+
+    public static void main(String[] args)
+    {
+        CropRatio cropRatio = new CropRatio();
+
+        cropRatio.add("Wheat", 4);
+        cropRatio.add("Wheat", 5);
+        cropRatio.add("Rice", 1);
+
+        System.out.println("Fraction of wheat: " + cropRatio.proportion("Wheat"));
+        System.out.println("Fraction of rice: " + cropRatio.proportion("Rice"));
+    }
+
+}
---
+++ MyJavaPlayground/src/sorting/MergeSort.java
@@ -0,0 +1,9 @@
+package sorting;
+
+/**
+ * Mergesort guarantees to sort an array of N items in time proportional to N log N, no matter what the input. 
+ * Its prime disadvantage is that it uses extra space proportional to N.
+ */
+public class MergeSort {
+    
+}
