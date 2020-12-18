79e1cade1e71fd1e68f0c30579abe112415e8e51
Harsha Edupuganti <explorer436@tutanota.com>
2020-12-18 15:53:58 -0500

    validate if a binary tree is a binary search tree

---
+++ Playground/src/datastructures/trees/ValidateBinarySearchTree.hs
@@ -0,0 +1,62 @@
+import MyBinaryTree
+
+{- |
+    Hi, here's your problem today. This problem was recently asked by Facebook:
+    
+    You are given the root of a binary search tree. Return true if it is a valid binary search tree, and false otherwise.
+    Recall that a binary search tree has the property that all values in the left subtree are less than or equal to the root, 
+    and all values in the right subtree are greater than or equal to the root.
+    
+    Here's a starting point:
+    
+    class TreeNode:
+      def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.key = key
+    
+    def is_bst(root):
+      # Fill this in.
+    
+    a = TreeNode(5)
+    a.left = TreeNode(3)
+    a.right = TreeNode(7)
+    a.left.left = TreeNode(1)
+    a.left.right = TreeNode(4)
+    a.right.left = TreeNode(6)
+    print is_bst(a)
+    
+    #    5
+    #   / \
+    #  3   7
+    # / \ /
+    #1  4 6
+-}
+
+validate EmptyTree = False
+validate (Node a EmptyTree EmptyTree) = True
+validate (Node a left EmptyTree) = (rootValue left < a) && validate left
+validate (Node a EmptyTree right) = (a < rootValue right) && validate right
+validate (Node a left right) = (rootValue left < a) && (a < rootValue right) && validate left && validate right
+
+test01 = validate (Node 5 
+                        (Node 3
+                              (Node 1 EmptyTree EmptyTree)
+                              (Node 4 EmptyTree EmptyTree)
+                        )
+                        (Node 7
+                              (Node 6 EmptyTree EmptyTree)
+                              EmptyTree
+                        )
+                  ) -- expect True
+
+test02 = validate (Node 5 
+                        (Node 3
+                              (Node 1 EmptyTree EmptyTree)
+                              (Node 4 EmptyTree EmptyTree)
+                        )
+                        (Node 7
+                              (Node 8 EmptyTree EmptyTree)
+                              EmptyTree
+                        )
+                  ) -- expect False
