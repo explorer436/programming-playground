7e07a252f97260335398862e25e235f9617b9750
Harsha Edupuganti <explorer436@tutanota.com>
2021-05-09 18:52:44 -0400

    different definition for height balanced binary tree

--- haskell-playground/src/Datastructures/Trees/HeightBalancedBinaryTree.hs
+++ haskell-playground/src/Datastructures/Trees/HeightBalancedBinaryTree.hs
@@ -39,16 +39,7 @@
 -}
 
 {- |
-   A tree where no leaf is much farther away from the root than any other leaf.
-   Different balancing schemes allow different definitions of “much farther” and different amounts of work to keep them balanced.
-   Consider a height-balancing scheme where following conditions should be checked to determine if a binary tree is balanced. 
-
-   An empty tree is height-balanced. A non-empty binary tree T is balanced if: 
-   1) Left subtree of T is balanced 
-   2) Right subtree of T is balanced 
-   3) The difference between heights of left subtree and right subtree is not more than 1. 
-
-   The above height-balancing scheme is used in AVL trees.
+   A height-balanced binary tree is defined as: a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 -}
 
 module Datastructures.Trees.HeightBalancedBinaryTree (heightBalancedBinaryTree) where
