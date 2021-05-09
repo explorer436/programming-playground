462ce7e94936fa646a7606b82ce20717091bd0d6
Harsha Edupuganti <explorer436@tutanota.com>
2021-05-09 18:53:39 -0400

    making the test cases clear

--- haskell-playground/test/Datastructures/Trees/HeightBalancedBinaryTreeSpec.hs
+++ haskell-playground/test/Datastructures/Trees/HeightBalancedBinaryTreeSpec.hs
@@ -7,18 +7,25 @@ import Datastructures.Trees.MyBinaryTree (Tree (..))
 spec :: Spec
 spec = do
   describe "heightBalancedBinaryTree" $ do
+
     it "returns True when the input is EmptyTree" $
       heightBalancedBinaryTree EmptyTree `shouldBe` True
+
     it "returns True when the input is a tree with just a root node" $
       heightBalancedBinaryTree (Node 3 EmptyTree EmptyTree) `shouldBe` True
+
     it "returns True when the height of left tree is 0 and height of right tree is -1" $
       heightBalancedBinaryTree (Node 3 (Node 4 EmptyTree EmptyTree) EmptyTree) `shouldBe` True
+
     it "returns True when the height of left tree is -1 and height of right tree is 0" $
       heightBalancedBinaryTree (Node 3 EmptyTree  (Node 5 EmptyTree EmptyTree)) `shouldBe` True
+
     it "returns True when the height of left tree is 0 and height of right tree is 0" $
       heightBalancedBinaryTree (Node 3 (Node 4 EmptyTree EmptyTree)  (Node 5 EmptyTree EmptyTree)) `shouldBe` True
+
     it "returns True when the height of left tree is 1 and height of right tree is 0" $
       heightBalancedBinaryTree (Node 3 (Node 4 (Node 2 EmptyTree EmptyTree) EmptyTree)  (Node 5 EmptyTree EmptyTree)) `shouldBe` True
+
     it "returns False when the height of left tree is 2 and height of right tree is 0" $
       heightBalancedBinaryTree (Node 3 (Node 4 (Node 2 (Node 1 EmptyTree EmptyTree) EmptyTree) EmptyTree)  (Node 5 EmptyTree EmptyTree)) `shouldBe` False
 
