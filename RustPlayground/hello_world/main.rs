30191d202fe52b21248cd779eab42b4d7b7b3c74
explorer436 <explorer436@tutanota.com>
2022-05-22 21:44:14 -0400

    my first rust program

---
+++ RustPlayground/hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// The main function is special: it is always the first code that runs in every executable Rust program.
+//    The first line declares a function named main that has no parameters and returns nothing.
+
+// This example does not use Cargo.
