d7e473913d966cdbd56f318559a25d0c8d03fe63
explorer436 <explorer436@tutanota.com>
2022-05-28 12:50:41 -0400

    updates to guessing game

--- RustPlayground/guessing_game/src/main.rs
+++ RustPlayground/guessing_game/src/main.rs
@@ -1,16 +1,43 @@
 use std::io;
 // The io library comes from the standard library, known as std.
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess!");
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-	.expect("Failed to read line");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("You guessed: {}", guess);
+    // println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess!");
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+    	.expect("Failed to read line");
+    
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+       let guess: u32 = match guess.trim().parse() {
+           Ok(num) => num,
+            Err(_) => continue,
+       };
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+		break;
+	    }
+        }
+    }
+
+    
 }
 
 // The full job of read_line is to take whatever the user types into standard input and append that into a string (without overwriting its contents), so we therefore pass that string as an argument. The string argument needs to be mutable so the method can change the string’s content.
