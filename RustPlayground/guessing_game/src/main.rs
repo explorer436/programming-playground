1178dffedd5a87b5b0d9ae860fe91d6935a9531e
explorer436 <explorer436@tutanota.com>
2022-08-09 14:21:53 -0400

    update maven notes

--- RustPlayground/guessing_game/src/main.rs
+++ RustPlayground/guessing_game/src/main.rs
@@ -36,8 +36,6 @@ fn main() {
 	    }
         }
     }
-
-    
 }
 
 // The full job of read_line is to take whatever the user types into standard input and append that into a string (without overwriting its contents), so we therefore pass that string as an argument. The string argument needs to be mutable so the method can change the string’s content.
