80b7f5495faf8068a978dacb3bf28be88b9bc77e
explorer436 <explorer436@tutanota.com>
2023-02-08 16:28:02 -0500

    update rust notes

--- rust-playground/variables/src/main.rs
+++ rust-playground/variables/src/main.rs
@@ -1,6 +1,28 @@
 fn main() {
+    example_use_case_for_mut_keyword();
+    example_use_case_for_shadowing();
+}
+
+fn example_use_case_for_mut_keyword() {
+    println!(">>> example_use_case_for_mut_keyword");
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    println!("<<< example_use_case_for_mut_keyword");
+}
+
+fn example_use_case_for_shadowing() {
+    println!(">>> example_use_case_for_shadowing");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x in the outer scope is: {}", x);
+    println!("<<< example_use_case_for_shadowing");
 }
