3bc0a59dcc9b2f6a84741900766fcd2b08aae0a0
explorer436 <explorer436@tutanota.com>
2023-05-14 05:02:49 -0400

    state pattern sample

---
+++ java-playground/design-pattern-samples/state-pattern-samples/src/main/java/com/my/company/statepattern/withpattern/CandyVendingMachineState.java
@@ -0,0 +1,10 @@
+package com.my.company.statepattern.withpattern;
+
+public interface CandyVendingMachineState {
+    void insertCoin();
+
+    void pressButton();
+
+    void dispense();
+}
+
---
+++ java-playground/design-pattern-samples/state-pattern-samples/src/main/java/com/my/company/statepattern/withpattern/NoCandyState.java
@@ -0,0 +1,29 @@
+package com.my.company.statepattern.withpattern;
+
+public class NoCandyState implements CandyVendingMachineState {
+    CandyVendingMachine machine;
+
+    public NoCandyState(CandyVendingMachine machine) {
+        this.machine = machine;
+    }
+
+    @Override
+    public void insertCoin() {
+        System.out.println("No candies available");
+    }
+
+    @Override
+    public void pressButton() {
+        System.out.println("No candies available");
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("No candies available");
+    }
+
+    @Override
+    public String toString() {
+        return "NoCandyState";
+    }
+}
