f26ab994ff5593a7e718630ef76cc1d3fbb1e823
explorer436 <explorer436@tutanota.com>
2023-06-15 00:31:48 -0400

    command line runner poc

---
+++ java-playground/springboot-commandlinerunner-example/src/test/java/com/example/springbootcommandlinerunnerexample/SpringbootCommandlinerunnerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcommandlinerunnerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommandlinerunnerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
