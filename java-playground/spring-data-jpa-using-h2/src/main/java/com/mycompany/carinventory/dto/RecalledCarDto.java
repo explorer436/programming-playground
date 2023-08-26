4955336548d63a13056bbbce98e0bb51ef286e44
explorer436 <explorer436@tutanota.com>
2023-08-26 12:44:30 -0400

    update JPA examples

---
+++ java-playground/spring-data-jpa-using-h2/src/main/java/com/mycompany/carinventory/dto/RecalledCarDto.java
@@ -0,0 +1,11 @@
+package com.mycompany.carinventory.dto;
+
+public interface RecalledCarDto {
+
+    String getRCID();
+    String getRCNAME();
+    String getRCREASONFORRECALL();
+    String getCID();
+    String getCPRICE();
+    String getCQUANTITY();
+}
