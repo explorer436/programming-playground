f20172176cea15f1a352085f369df40e445e2f35
explorer436 <explorer436@tutanota.com>
2023-09-19 12:19:48 -0400

    pushing latest updates

--- java-playground/spring-data-file-io-poc/src/main/java/com/example/springfileiodemo/SpringFileIoDemoApplication.java
+++ java-playground/spring-data-file-io-poc/src/main/java/com/example/springfileiodemo/SpringFileIoDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringFileIoDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFileIoDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileIoDemoApplication.class, args);
+    }
 
 }
