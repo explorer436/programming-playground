da8da6e36ecf0baad25a9799dc7a89abba855123
explorer436 <explorer436@tutanota.com>
2023-12-29 00:15:56 -0500

    adding application that uses elasticsearch-java

---
+++ java-playground/springboot-elasticsearch/elasticsearch-java-api-client/src/main/java/com/example/elasticsearchjavaapiclient/ElasticsearchJavaApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchjavaapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchJavaApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchJavaApiClientApplication.class, args);
+	}
+
+}
