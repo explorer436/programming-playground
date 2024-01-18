4e74a829cbdd4875b769830aaee123f3f260c26a
explorer436 <explorer436@tutanota.com>
2024-01-18 13:50:41 -0500

    update strategy pattern example

--- java-playground/design-pattern-samples/design-pattern-examples/src/main/java/com/my/company/strategypattern/notificationexample/factory/NotificationFactory.java
+++ java-playground/design-pattern-samples/design-pattern-examples/src/main/java/com/my/company/strategypattern/notificationexample/factory/NotificationFactory.java
@@ -1,26 +1,24 @@
 package com.my.company.strategypattern.notificationexample.factory;
 
 import com.my.company.strategypattern.notificationexample.NotificationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class NotificationFactory {
+
+    /**
+     * Since each strategy (in impl directory) is marked as a Service with @Service annotation, we can get all the concrete implementations of NotificationService interface by Autowiring NotificationService.
+     * This will pick all the implementations of NotificationService in a Map, by setting each implementation against the provided Bean name in the @Service annotation.
+     * Uses constructor-autowiring to create a new NotificationFactory instance with the provided map of NotificationService instances.
+     */
 
     private final Map<String, NotificationService> notificationServiceMap;
 
-    /**
-     * Uses constructor-autowiring to create a new NotificationFactory instance
-     * with the provided map of NotificationService instances.
-     *
-     * @param notificationServices a map of NotificationService instances
-     */
-    public NotificationFactory(Map<String, NotificationService> notificationServices) {
-        this.notificationServiceMap = notificationServices;
-    }
-
-    public NotificationService getNotificationService(String notificationType) {
+    private NotificationService getNotificationService(String notificationType) {
         NotificationService notificationService = notificationServiceMap.get(notificationType);
         if (notificationService == null) {
             throw new RuntimeException("Unsupported notification type");
