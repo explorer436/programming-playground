f4515a7fe81845980f5d44c31a11719e0b94428a
explorer436 <explorer436@tutanota.com>
2024-04-23 15:44:50 -0400

    open rewrite example

---
+++ java-playground/openrewrite-poc/openrewrite-demo-1/src/test/java/com/my/company/openrewritedemo1/OpenrewriteDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.company.openrewritedemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrewriteDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ java-playground/openrewrite-poc/openrewrite-demo-2/src/main/java/com/my/company/openrewritedemo1/OpenrewriteDemo2Application.java
@@ -0,0 +1,14 @@
+package com.my.company.openrewritedemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrewriteDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenrewriteDemo2Application.class, args);
+		System.out.println("app started...");
+	}
+
+}
