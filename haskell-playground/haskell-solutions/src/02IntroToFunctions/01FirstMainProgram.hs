2df4b2f95edc80fc02f0e5aae9ae156bfe6ef00d
explorer436 <explorer436@tutanota.com>
2024-06-15 17:12:45 -0400

    update haskell notes

--- haskell-playground/haskell-solutions/src/02IntroToFunctions/01FirstMainProgram.hs
+++ haskell-playground/haskell-solutions/src/02IntroToFunctions/01FirstMainProgram.hs
@@ -1,5 +1,18 @@
 {- |
   To run this, load this file into ghci and call `main`
+
+  Getting help : Enter :? at the ghci prompt
+
+  To exit or quit out of ghci : Ctrl D
+
+  Open terminal and type ghci to launch ghci to go into GHCI!
+  The prompt here is Prelude> and that will work for minor functions.
+  If you want to write your functions in a file, write them in a file called 'baby.hs'.
+  To load this file into ghci, type `:l baby`
+  If you change the .hs script, just run `:l baby` again or do `:r`, 
+  which is equivalent because it reloads the current script.
+  To unload this file from ghci, type `:m` (short for module).
+  This will take you back to Prelude> 
 -}
 
 helloString :: String
@@ -8,5 +21,8 @@ helloString = "Hello world!"
 main :: IO ()
 main = putStrLn helloString
 
-----------------------------------------------------------------
+-- After loading the file into ghci, just call the name of the method (and pass in arguments if necessary).
+-- > helloworld
 
+-- What is the type signature of a function? Just ask ghci
+-- > :t largestDivisible
