be12575da4fbe686c9997f4c06e68c3bcf441fdb
explorer436 <explorer436@tutanota.com>
2024-08-12 11:22:27 -0400

    refactor file names

--- java-playground/my-java-solutions/src/main/java/com/my/company/strings/SemanticVersionComparator.java
+++ java-playground/my-java-solutions/src/main/java/com/my/company/strings/SemanticVersionComparator.java
@@ -6,14 +6,14 @@ package com.my.company.strings;
  * It should return -1 if version1 < version2, 1 if version1 > version2, and 0 if they are equal.
  * Note: you can assume all versions will follow the [major].[minor].[patch] format
  */
-public class VersionComparison {
+public class SemanticVersionComparator {
     // "3.5.18" = "3.5.18" = equality condition
 
     // "3.5.1" > "2.5.1" = major version check
     // "3.5.1" > "3.4.1" = minor version check
     // "3.5.1" > "3.5.0" = patch version check
 
-    public int compareVersions(String str1, String str2) {
+    public int compare(String str1, String str2) {
         if (str1.equals(str2)) {
             return 0;
         }
@@ -21,25 +21,17 @@ public class VersionComparison {
         String[] str1Parts = str1.split("\\.");
         String[] str2Parts = str2.split("\\.");
 
-        int majorVersionComparisonResult = extracted(str1Parts[0], str2Parts[0]);
-        if (majorVersionComparisonResult != 0) {
-            return majorVersionComparisonResult;
-        }
-
-        int minorVersionComparisonResult = extracted(str1Parts[1], str2Parts[1]);
-        if (minorVersionComparisonResult != 0) {
-            return minorVersionComparisonResult;
-        }
-
-        int patchVersionComparisonResult = extracted(str1Parts[2], str2Parts[2]);
-        if (patchVersionComparisonResult != 0) {
-            return patchVersionComparisonResult;
+        for (int i=0; i <= str1Parts.length; i++) {
+            int comparisonResult = helper(str1Parts[i], str2Parts[i]);
+            if (comparisonResult != 0) {
+                return comparisonResult;
+            }
         }
 
         return -1;
     }
 
-    private static int extracted(String str1Part, String str2Part) {
+    private static int helper(String str1Part, String str2Part) {
 
         if (Integer.parseInt(str1Part) == Integer.parseInt(str2Part)) {
             return 0;
--- java-playground/my-java-solutions/src/test/java/com/my/company/strings/SemanticVersionComparatorTests.java
+++ java-playground/my-java-solutions/src/test/java/com/my/company/strings/SemanticVersionComparatorTests.java
@@ -4,49 +4,49 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class VersionComparisonTests {
+public class SemanticVersionComparatorTests {
 
-    VersionComparison versionComparison = new VersionComparison();
+    SemanticVersionComparator semanticVersionComparator = new SemanticVersionComparator();
 
     @Test
     void test_equality() {
-        int actual = versionComparison.compareVersions("3.5.18", "3.5.18");
+        int actual = semanticVersionComparator.compare("3.5.18", "3.5.18");
         assertEquals(0, actual);
     }
 
     @Test
     void test_major_version_is_greater() {
-        int actual = versionComparison.compareVersions("3.5.18", "2.5.18");
+        int actual = semanticVersionComparator.compare("3.5.18", "2.5.18");
         assertEquals(1, actual);
     }
 
     @Test
     void test_minor_version_is_greater() {
-        int actual = versionComparison.compareVersions("3.5.18", "3.4.18");
+        int actual = semanticVersionComparator.compare("3.5.18", "3.4.18");
         assertEquals(1, actual);
     }
 
     @Test
     void test_patch_version_is_greater() {
-        int actual = versionComparison.compareVersions("3.5.1", "3.5.0");
+        int actual = semanticVersionComparator.compare("3.5.1", "3.5.0");
         assertEquals(1, actual);
     }
 
     @Test
     void test_major_version_is_lesser() {
-        int actual = versionComparison.compareVersions("3.5.18", "4.5.18");
+        int actual = semanticVersionComparator.compare("3.5.18", "4.5.18");
         assertEquals(-1, actual);
     }
 
     @Test
     void test_minor_version_is_lesser() {
-        int actual = versionComparison.compareVersions("3.5.18", "3.6.18");
+        int actual = semanticVersionComparator.compare("3.5.18", "3.6.18");
         assertEquals(-1, actual);
     }
 
     @Test
     void test_patch_version_is_lesser() {
-        int actual = versionComparison.compareVersions("3.5.1", "3.5.2");
+        int actual = semanticVersionComparator.compare("3.5.1", "3.5.2");
         assertEquals(-1, actual);
     }
 }
