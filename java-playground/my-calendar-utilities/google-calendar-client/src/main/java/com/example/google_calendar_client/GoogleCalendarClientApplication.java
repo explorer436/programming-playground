54d755615294572af6a7525e9dd3b7f6f15db3e6
explorer436 <explorer436@tutanota.com>
2024-09-19 12:00:14 -0400

    update calendar api client applications

---
+++ java-playground/my-calendar-utilities/google-calendar-client/src/main/java/com/example/google_calendar_client/GoogleCalendarClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.google_calendar_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCalendarClientApplication.class, args);
+	}
+
+}
