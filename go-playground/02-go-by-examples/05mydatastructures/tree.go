0f17469f5fc25aebda9a629e08d3c1f04369ba4b
explorer436 <explorer436@tutanota.com>
2025-01-07 17:48:02 -0500

    changes from explorer436-p50-20eqs27p03 on Tue Jan 7 05:48:01 PM EST 2025

--- go-playground/02-go-by-examples/05mydatastructures/tree.go
+++ go-playground/02-go-by-examples/05mydatastructures/tree.go
@@ -18,31 +18,57 @@ type BinaryTree struct {
 	Right *BinaryTree
 }
 
+type BinarySearchNode struct {
+	Value int
+	Left  *BinarySearchTree
+	Right *BinarySearchTree
+}
+
 type BinarySearchTree struct {
 	Value int
 	Left  *BinarySearchTree
 	Right *BinarySearchTree
 }
 
-func CreateBinarySearchTree(x int) BinarySearchTree {
-
-	return BinarySearchTree{
-		Value: x,
-	}
-}
-
 func InsertInBinarySearchTree(x int, bst BinarySearchTree) BinarySearchTree {
+	if bst == (BinarySearchTree{}) {
+		return BinarySearchTree{
+			Value: x,
+		}
+	}
 	if bst.Value == x {
 		// It is a duplicate.
 		// We don't want to support duplicates. So, don't do anything.
 		return bst
 	}
-	if bst.Value < x {
-		return
-	}
-	if bst.Value > x {
+	if x < bst.Value {
+		var newLeft BinarySearchTree
+		if bst.Left == nil {
+			newLeft = InsertInBinarySearchTree(x, BinarySearchTree{})
 
+		} else {
+			newLeft = InsertInBinarySearchTree(x, *bst.Left)
+		}
+		return BinarySearchTree{
+			Value: bst.Value,
+			Left:  &newLeft,
+			Right: bst.Right,
+		}
 	}
+	if x > bst.Value {
+		var newRight BinarySearchTree
+		if bst.Right == nil {
+			newRight = InsertInBinarySearchTree(x, BinarySearchTree{})
+		} else {
+			newRight = InsertInBinarySearchTree(x, *bst.Right)
+		}
+		return BinarySearchTree{
+			Value: bst.Value,
+			Left:  bst.Left,
+			Right: &newRight,
+		}
+	}
+	return BinarySearchTree{}
 }
 
 // https://go.dev/tour/concurrency/7
--- go-playground/02-go-by-examples/05mydatastructures/tree_test.go
+++ go-playground/02-go-by-examples/05mydatastructures/tree_test.go
@@ -1,10 +1,13 @@
 package mydatastructures
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestCreateBinarySearchTree(t *testing.T) {
+func Test_InsertInBinarySearchTree_InsertInEmptyTree(t *testing.T) {
 
-	got := CreateBinarySearchTree(789)
+	var got BinarySearchTree = InsertInBinarySearchTree(789, BinarySearchTree{})
 
 	want := BinarySearchTree{
 		Value: 789,
@@ -13,4 +16,96 @@ func TestCreateBinarySearchTree(t *testing.T) {
 	if got != want {
 		t.Errorf("Result was incorrect - got %+v, but want %+v", got, want)
 	}
+}
+
+func Test_InsertInBinarySearchTree_InsertDuplicateElement(t *testing.T) {
+
+	var got BinarySearchTree = InsertInBinarySearchTree(789, BinarySearchTree{
+		Value: 789,
+	})
+
+	want := BinarySearchTree{
+		Value: 789,
+	}
+
+	if got != want {
+		t.Errorf("Result was incorrect - got %+v, but want %+v", got, want)
+	}
+}
+
+func Test_InsertInBinarySearchTree_2(t *testing.T) {
+
+	var got BinarySearchTree = InsertInBinarySearchTree(15,
+		InsertInBinarySearchTree(27,
+			InsertInBinarySearchTree(30,
+				InsertInBinarySearchTree(29,
+					InsertInBinarySearchTree(26,
+						InsertInBinarySearchTree(22,
+							InsertInBinarySearchTree(32,
+								InsertInBinarySearchTree(20,
+									InsertInBinarySearchTree(25,
+										InsertInBinarySearchTree(17, BinarySearchTree{}),
+									)))))))))
+
+	// Do not use.
+	// This is a tedious process and prone to errors.
+	var cases = []struct {
+		expected int
+		received int
+	}{
+		// the table itself
+		{17, got.Value},
+		{15, got.Left.Value},
+		{25, got.Right.Value},
+		{20, got.Right.Left.Value},
+		{22, got.Right.Left.Right.Value},
+		{32, got.Right.Right.Value},
+		{26, got.Right.Right.Left.Value},
+		{29, got.Right.Right.Left.Right.Value},
+		{27, got.Right.Right.Left.Right.Left.Value},
+		{30, got.Right.Right.Left.Right.Right.Value},
+	}
+
+	for _, c := range cases {
+		if !reflect.DeepEqual(c.expected, c.received) {
+			t.Errorf("Result was incorrect - got %+v, but want %+v", c.received, c.expected)
+		}
+	}
+
+	// Building a complex struct and using it for equality testing
+	var expected BinarySearchTree = BinarySearchTree{
+		Value: 17,
+		Left: &BinarySearchTree{
+			Value: 15,
+		},
+		Right: &BinarySearchTree{
+			Value: 25,
+			Left: &BinarySearchTree{
+				Value: 20,
+				Right: &BinarySearchTree{
+					Value: 22,
+				},
+			},
+			Right: &BinarySearchTree{
+				Value: 32,
+				Left: &BinarySearchTree{
+					Value: 26,
+					Right: &BinarySearchTree{
+						Value: 29,
+						Left: &BinarySearchTree{
+							Value: 27,
+						},
+						Right: &BinarySearchTree{
+							Value: 30,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Result was incorrect - got %+v, but want %+v", got, expected)
+	}
+
 }
