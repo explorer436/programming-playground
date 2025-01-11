ae6a3f3c2753b7a5f010105d1455e0069ab602f2
explorer436 <explorer436@tutanota.com>
2025-01-10 23:42:45 -0500

    update notes

--- go-playground/02-go-by-examples/05mydatastructures/tree.go
+++ go-playground/02-go-by-examples/05mydatastructures/tree.go
@@ -24,46 +24,5 @@ type BinarySearchTree struct {
 	Right *BinarySearchTree
 }
 
-func InsertInBinarySearchTree(x int, bst BinarySearchTree) BinarySearchTree {
-	if bst == (BinarySearchTree{}) {
-		return BinarySearchTree{
-			Value: x,
-		}
-	}
-	if bst.Value == x {
-		// It is a duplicate.
-		// We don't want to support duplicates. So, don't do anything.
-		return bst
-	}
-	if x < bst.Value {
-		var newLeft BinarySearchTree
-		if bst.Left == nil {
-			newLeft = InsertInBinarySearchTree(x, BinarySearchTree{})
-
-		} else {
-			newLeft = InsertInBinarySearchTree(x, *bst.Left)
-		}
-		return BinarySearchTree{
-			Value: bst.Value,
-			Left:  &newLeft,
-			Right: bst.Right,
-		}
-	}
-	if x > bst.Value {
-		var newRight BinarySearchTree
-		if bst.Right == nil {
-			newRight = InsertInBinarySearchTree(x, BinarySearchTree{})
-		} else {
-			newRight = InsertInBinarySearchTree(x, *bst.Right)
-		}
-		return BinarySearchTree{
-			Value: bst.Value,
-			Left:  bst.Left,
-			Right: &newRight,
-		}
-	}
-	return BinarySearchTree{}
-}
-
 // https://go.dev/tour/concurrency/7
 // https://go.dev/tour/concurrency/8
