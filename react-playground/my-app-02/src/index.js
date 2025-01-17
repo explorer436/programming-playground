976de784c2cf282f7780814371543438b9959675
explorer436 <explorer436@tutanota.com>
2025-01-16 23:34:54 -0500

    update react notes

---
+++ react-playground/my-app-02/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Greeting = () => {
+  return <h2>My First Component</h2>;
+};
+
+/* const Greeting = () => {
+  return React.createElement("h2", {}, "hello world");
+}; */
+
+/* const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "hello world")
+  );
+}; */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greeting />);
