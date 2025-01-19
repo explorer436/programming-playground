b561cfa52fc79b9509d6912d4b7270795b487800
explorer436 <explorer436@tutanota.com>
2025-01-19 00:09:08 -0500

    changes from explorer436-p50-20eqs27p03 on Sun Jan 19 12:09:08 AM EST 2025

--- react-playground/my-app-02/src/index.js
+++ react-playground/my-app-02/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Greeting = () => {
+/* const Greeting = () => {
   return <h2>My First Component</h2>;
-};
+}; */
 
 /* const Greeting = () => {
   return React.createElement("h2", {}, "hello world");
@@ -16,6 +16,23 @@ const Greeting = () => {
     React.createElement("h2", {}, "hello world")
   );
 }; */
+
+function Greeting() {
+  return (
+    <>
+      <div className="someValue">
+        <h3>hello people</h3>
+        <ul>
+          <li>
+            <a href="#">hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+      <input type="text" name="" id="" />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Greeting />);
