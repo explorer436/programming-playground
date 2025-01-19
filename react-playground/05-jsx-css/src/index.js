11135ace1f828276dd1b033eca6656f89ed26d66
explorer436 <explorer436@tutanota.com>
2025-01-19 12:32:31 -0500

    changes from explorer436-legion-82b1 on Sun Jan 19 12:32:30 PM EST 2025

--- react-playground/05-jsx-css/src/index.js
+++ react-playground/05-jsx-css/src/index.js
@@ -27,8 +27,22 @@ const Book = () => {
 // right click on the image in the website and select "copy image address"
 const Image = () => <img src="./images/91ulu+khYLL._AC_UL600_SR600,400_.jpg" />;
 const Title = () => <h2>The Frozen River: A GMA Book Club Pick: A Novel</h2>;
+
+/* const Author = () => {
+  return (
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+      Ariel Lawhon
+    </h4>
+  );
+}; */
+
 const Author = () => {
-  return <h4>Ariel Lawhon</h4>;
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>Ariel Lawhon </h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
