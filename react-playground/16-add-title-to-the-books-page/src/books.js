a65b60476f4b65436981d640135b3c8c901354c5
explorer436 <explorer436@tutanota.com>
2025-01-24 16:48:02 -0500

    changes from explorer436-p50-20eqs27p03 on Fri Jan 24 04:48:01 PM EST 2025

---
+++ react-playground/16-add-title-to-the-books-page/src/books.js
@@ -0,0 +1,16 @@
+import img1 from "./images/91ulu+khYLL._AC_UL600_SR600,400_.jpg";
+
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+  },
+];
