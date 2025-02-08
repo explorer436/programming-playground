2d74e2ea2fbcb9342e00af69db1d99768f2a1aa9
explorer436 <explorer436@tutanota.com>
2025-02-08 12:12:22 -0500

    update notes

---
+++ react-playground/26-birthday-buddy/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import List from './List';
+import {data} from "./data.js";
+function App() {
+    const [people, setPeople] = useState(data);
+    return (
+        <main>
+            <section className='container'>
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button
+                    type='button'
+                    className='btn btn-block'
+                    onClick={() => setPeople([])}
+                >
+                    clear all
+                </button>
+            </section>
+        </main>
+    );
+}
+
+export default App;
