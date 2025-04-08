23d2f70d7144cdbe2ca268a4464a1cc955cf9dba
explorer436 <explorer436@tutanota.com>
2025-04-08 15:40:33 -0400

    changes from explorer436-p50-20eqs27p03 on Tue Apr 8 03:40:33 PM EDT 2025

--- go-playground/14-parse-an-expression-into-json/main.go
+++ go-playground/14-parse-an-expression-into-json/main.go
@@ -1,122 +1,126 @@
 package main
 
+import (
+	"errors"
+	"regexp"
+	"slices"
+	"strings"
+)
+
 func main() {
-
-
 
 }
 
 func TransformExpression(exp string) (interface{}, error) {
-  tokens, err := GetTokensFromExpression(exp)
-  if err != nil {
-    return nil, err
-  }
-  transformedExp, err := CreateJSONTreeFromTokens(tokens)
-  if err != nil {
-    return nil, err
-  }
-  return transformedExp, nil
+	tokens, err := GetTokensFromExpression(exp)
+	if err != nil {
+		return nil, err
+	}
+	transformedExp, err := CreateJSONTreeFromTokens(tokens)
+	if err != nil {
+		return nil, err
+	}
+	return transformedExp, nil
 }
 
 func GetTokensFromExpression(exp string) ([]string, error) {
-  expression = strings.ToUpper(exp)
+	expression := strings.ToUpper(exp)
 
-  // Special cases
-  exceptions := []string{"ALWAYS","TRUE", "FALSE", "NO"}
-  if slices.Contains(exceptions, expression) {
-    if expression == "TRUE" || expression == "ALWAYS" {
-      return []string{"true"}, nil
-    }
-    // else, similarly handle other cases
-  }
+	// Special cases
+	exceptions := []string{"ALWAYS", "TRUE", "FALSE", "NO"}
+	if slices.Contains(exceptions, expression) {
+		if expression == "TRUE" || expression == "ALWAYS" {
+			return []string{"true"}, nil
+		}
+		// else, similarly handle other cases
+	}
 
-  tokensFromComplexExpressions, err := GetTokensFromComplexExpressions(expression)
-  if err != nil {
-    return nil, err
-  }
-  if tokensFromComplexExpressions == nil || len(tokensFromComplexExpressions) == 0 {
-    return nil, errors.New("invalid expression")
-  }
-  return tokensFromComplexExpressions, nil
+	tokensFromComplexExpressions, err := GetTokensFromComplexExpressions(expression)
+	if err != nil {
+		return nil, err
+	}
+	if tokensFromComplexExpressions == nil || len(tokensFromComplexExpressions) == 0 {
+		return nil, errors.New("invalid expression")
+	}
+	return tokensFromComplexExpressions, nil
 }
 
 func GetTokensFromComplexExpressions(expression string) ([]string, error) {
 
-  rules := []struct {
-    pattern *regexp.Regexp
-    action func(string) string
-  }{
-    {
-      pattern: regexp.MustCompile(`(?m)^[(][\w\-\#\+]{5}[{1})]`),
-      action: func(token string) string {
-	return token[1 : len(token) - 1]
-      }
-    },
-    {
-      // same as the previous - but without the square brackets
-      pattern: regexp.MustCompile(`(?m)^[\w\-\#\+]{5}`),
-      action: func(token string) string {
-	return token
-      }
-    },
-    {
-      pattern: regexp.MustCompile(`(?m)^!+`),
-      action: func(token string) string {
-	return "!"
-      }
-    },
-    {
-      pattern: regexp.MustCompile(`(?m)^&+`),
-      action: func(token string) string {
-	return "&"
-      }
-    },
-    {
-      pattern: regexp.MustCompile(`(?m)^[|]+`),
-      action: func(token string) string {
-	return "|"
-      }
-    },
-    {
-      pattern: regexp.MustCompile(`(?m)^\(`),
-      action: func(token string) string {
-	return "("
-      }
-    },
-    {
-      pattern: regexp.MustCompile(`(?m)^\)`),
-      action: func(token string) string {
-	return ")"
-      }
-    },
-    {
-      pattern: regexp.MustCompile(`(?m)^\s+`),
-      action: func(token string) string {
-	return ""
-      }
-    }
+	rules := []struct {
+		pattern *regexp.Regexp
+		action  func(string) string
+	}{
+		{
+			pattern: regexp.MustCompile(`(?m)^[(][\w\-\#\+]{5}[{1})]`),
+			action: func(token string) string {
+				return token[1 : len(token)-1]
+			},
+		},
+		{
+			// same as the previous - but without the square brackets
+			pattern: regexp.MustCompile(`(?m)^[\w\-\#\+]{5}`),
+			action: func(token string) string {
+				return token
+			},
+		},
+		{
+			pattern: regexp.MustCompile(`(?m)^!+`),
+			action: func(token string) string {
+				return "!"
+			},
+		},
+		{
+			pattern: regexp.MustCompile(`(?m)^&+`),
+			action: func(token string) string {
+				return "&"
+			},
+		},
+		{
+			pattern: regexp.MustCompile(`(?m)^[|]+`),
+			action: func(token string) string {
+				return "|"
+			},
+		},
+		{
+			pattern: regexp.MustCompile(`(?m)^\(`),
+			action: func(token string) string {
+				return "("
+			},
+		},
+		{
+			pattern: regexp.MustCompile(`(?m)^\)`),
+			action: func(token string) string {
+				return ")"
+			},
+		},
+		{
+			pattern: regexp.MustCompile(`(?m)^\s+`),
+			action: func(token string) string {
+				return ""
+			},
+		}}
 
-    var results []string
-    var remainder = expression
+	var results []string
+	var remainder = expression
 
-    for len(remainder) > 0 {
-       matched := false
-       for _, rule := range rules {
-	 if rule.pattern.MatchString(remainder) {
-	   match := rule.pattern.FindString(remainder)
-	   remainder := strings.TrimPrefix(remainder, match)
-	   if actionResult := rule.action(match); len(actionResult) > 0 {
-	     results = append(results, actionResult)
-	   }
-	   matched = true
-	   break
-	 }
-       }
-       if !matched {
-	 return nil, errors.New("error processing expression - incomplete expression found")
-	 break
-       }
-    }
-    return results, nil
-  }
+	for len(remainder) > 0 {
+		matched := false
+		for _, rule := range rules {
+			if rule.pattern.MatchString(remainder) {
+				match := rule.pattern.FindString(remainder)
+				remainder = strings.TrimPrefix(remainder, match)
+				if actionResult := rule.action(match); len(actionResult) > 0 {
+					results = append(results, actionResult)
+				}
+				matched = true
+				break
+			}
+		}
+		if !matched {
+			return nil, errors.New("error processing expression - incomplete expression found")
+			break
+		}
+	}
+	return results, nil
 }
