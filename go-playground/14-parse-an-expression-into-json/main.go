7f99d00959841ce803dd92257665d875a8729129
explorer436 <explorer436@tutanota.com>
2025-04-08 16:27:38 -0400

    changes from explorer436-p50-20eqs27p03 on Tue Apr 8 04:27:38 PM EDT 2025

--- go-playground/14-parse-an-expression-into-json/main.go
+++ go-playground/14-parse-an-expression-into-json/main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"log"
 	"regexp"
 	"slices"
 	"strings"
+	"unicode"
 )
 
 func main() {
-  tests := []struct {
-    input string
-    expected string
-  }{
-    {"abcde", "abcde"},
-    {"abc--&def--", "test"}
-  }
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"abcde", "\"ABCDE\""},
+		{"abc--&def--", "{\"and\":[\"ABC--\",\"DEF--\"]}"},
+	}
 
-  for _, test := range tests {
-    actual, _ := TransformExpression(test.input)
+	for _, test := range tests {
+		actual, _ := TransformExpression(test.input)
 
-    actualBytes, _ := json.Marshal(actual)
-    actualStr := string(actualBytes)
+		actualBytes, _ := json.Marshal(actual)
+		actualStr := string(actualBytes)
 
-    if (actualStr != test.expected) {
-      log.Error("TransformExpression(%s) = %s; expected %s", test.input, actualStr, test.expected)
-    }
-  }
+		if actualStr != test.expected {
+			log.Fatalf("TransformExpression(%s) = %s; expected %s", test.input, actualStr, test.expected)
+		}
+	}
 
 }
 
@@ -43,104 +46,104 @@ func TransformExpression(exp string) (interface{}, error) {
 
 func CreateJSONTreeFromTokens(tokens []string) (interface{}, error) {
 
-  jsonTree := make(map[string][]interface{})
-  var lastCondition interface{}
-  var lastOperator string
-  var secondToLastOperator string
-  operatorDict := map[string]string{"&": "and", "|": "or", "!": "not"}
-  i := 0
+	jsonTree := make(map[string][]interface{})
+	var lastCondition interface{}
+	var lastOperator string
+	var secondToLastOperator string
+	operatorDict := map[string]string{"&": "and", "|": "or", "!": "not"}
+	i := 0
 
-  if len(tokens) == 1 {
-    return tokens[0], nil
-  }
-
-  // Iterate through the tokens and generate a json tree
-  for i < len(tokens) {
-    token := tokens[i]
-
-    if isValidCode(token) {
-      lastCondition = token
-      if lastOperator == "not" && secondToLastOperator != "" {
-	notDict := map[string]interface{}{lastOperator: lastCondition}
-	jsonTree[secondToLastOperator] = append(jsonTree[secondToLastOperator], notDict)
-      }
-    } else if _, ok := operatorDict[token]; ok {
-      // Check if token is in operatorDict
-      // Save the operators in case there are multiple operators in a row
-      secondToLastOperator = lastOperator
-      lastOperator = operatorDict[token]
-      if lastCondition != nil && secondToLastOperator != "not" {
-	// Append the current code to the corresponding operator list
-	jsonTree[lastOperator] = append(jsonTree[lastOperator], lastCondition)
-	// Reset lastCondition since its already been processed
-	lastCondition = nil
-      }
-    } else if token == "(" {
-      // Find the index of the last ending paranthesis
-      endingParanthesisIndex := findLastParanthesisIndex(tokens, i)
-      if endingParanthesisIndex == -1 {
-	return nil, errors.New("error processing expression - missing or misplaced parantheses")
-      }
-      // Get the tokens between the paranthesis
-      tokensBetweenParanthesis := tokens[i+1 : endingParanthesisIndex]
-      // Recursively call CreateJSONTreeFromTokens to process the tokens between the paranthesis
-      treeBetweenParanthesis, _ := CreateJSONTreeFromTokens(tokensBetweenParanthesis)
-      if lastOperator == "not" {
-	if secondToLastOperator != "" {
-	  // Handle the case where there are multiple operators in a row
-	  notDict := map[string]interface{}{lastOperator: treeBetweenParanthesis}
-	  jsonTree[secondToLastOperator] = append(jsonTree[secondToLastOperator], notDict)
-	} else {
-	  jsonTree[lastOperator] = []interface{}{treeBetweenParanthesis}
+	if len(tokens) == 1 {
+		return tokens[0], nil
 	}
-      } else if lastOperator != "" {
-	// Check if lastOperator is not empty
-	jsonTree[lastOperator] = append(jsonTree[lastOperator], treeBetweenParanthesis)
-      } else {
-	lastCondition = treeBetweenParanthesis
-      }
 
-      // Set the index to the ending paranthesis index to skip the tokens between the paranthesis
-      i = endingParanthesisIndex
-    }
-    i++
-  }
+	// Iterate through the tokens and generate a json tree
+	for i < len(tokens) {
+		token := tokens[i]
 
-  // Handle the last condition if it hasn't been processed (i.e., no operator follows it)
-  if lastCondition != nil && lastOperator != "" {
-    jsonTree[lastOperator] = append(jsonTree[lastOperator], lastCondition)
-  }
+		if isValidCode(token) {
+			lastCondition = token
+			if lastOperator == "not" && secondToLastOperator != "" {
+				notDict := map[string]interface{}{lastOperator: lastCondition}
+				jsonTree[secondToLastOperator] = append(jsonTree[secondToLastOperator], notDict)
+			}
+		} else if _, ok := operatorDict[token]; ok {
+			// Check if token is in operatorDict
+			// Save the operators in case there are multiple operators in a row
+			secondToLastOperator = lastOperator
+			lastOperator = operatorDict[token]
+			if lastCondition != nil && secondToLastOperator != "not" {
+				// Append the current code to the corresponding operator list
+				jsonTree[lastOperator] = append(jsonTree[lastOperator], lastCondition)
+				// Reset lastCondition since its already been processed
+				lastCondition = nil
+			}
+		} else if token == "(" {
+			// Find the index of the last ending paranthesis
+			endingParanthesisIndex := findLastParanthesisIndex(tokens, i)
+			if endingParanthesisIndex == -1 {
+				return nil, errors.New("error processing expression - missing or misplaced parantheses")
+			}
+			// Get the tokens between the paranthesis
+			tokensBetweenParanthesis := tokens[i+1 : endingParanthesisIndex]
+			// Recursively call CreateJSONTreeFromTokens to process the tokens between the paranthesis
+			treeBetweenParanthesis, _ := CreateJSONTreeFromTokens(tokensBetweenParanthesis)
+			if lastOperator == "not" {
+				if secondToLastOperator != "" {
+					// Handle the case where there are multiple operators in a row
+					notDict := map[string]interface{}{lastOperator: treeBetweenParanthesis}
+					jsonTree[secondToLastOperator] = append(jsonTree[secondToLastOperator], notDict)
+				} else {
+					jsonTree[lastOperator] = []interface{}{treeBetweenParanthesis}
+				}
+			} else if lastOperator != "" {
+				// Check if lastOperator is not empty
+				jsonTree[lastOperator] = append(jsonTree[lastOperator], treeBetweenParanthesis)
+			} else {
+				lastCondition = treeBetweenParanthesis
+			}
 
-  return jsonTree, nil
+			// Set the index to the ending paranthesis index to skip the tokens between the paranthesis
+			i = endingParanthesisIndex
+		}
+		i++
+	}
+
+	// Handle the last condition if it hasn't been processed (i.e., no operator follows it)
+	if lastCondition != nil && lastOperator != "" {
+		jsonTree[lastOperator] = append(jsonTree[lastOperator], lastCondition)
+	}
+
+	return jsonTree, nil
 }
 
-func isValidCode(token string) book {
-  if len(token) != 5 {
-    return false
-  }
-  for _, char := range token {
-    if !unicode.IsLetter(char) && !unicode.IsDigit(char) && char != '-' && char != '#' {
-      return false
-    }
-  }
-  return true
+func isValidCode(token string) bool {
+	if len(token) != 5 {
+		return false
+	}
+	for _, char := range token {
+		if !unicode.IsLetter(char) && !unicode.IsDigit(char) && char != '-' && char != '#' {
+			return false
+		}
+	}
+	return true
 }
 
 func findLastParanthesisIndex(tokens []string, firstParanthesisIndex int) int {
-  count := 0
+	count := 0
 
-  for i := firstParanthesisIndex; i < len(tokens); i++ {
-    if tokens[i] == '(' {
-      count++
-    } else if tokens[i] == ')' {
-      count--
-      if count == 0 {
-	return i
-      }
-    }
-  }
-  // If the closing paranthesis is not found
-  return -1
+	for i := firstParanthesisIndex; i < len(tokens); i++ {
+		if tokens[i] == "(" {
+			count++
+		} else if tokens[i] == ")" {
+			count--
+			if count == 0 {
+				return i
+			}
+		}
+	}
+	// If the closing parenthesis is not found
+	return -1
 }
 
 func GetTokensFromExpression(exp string) ([]string, error) {
