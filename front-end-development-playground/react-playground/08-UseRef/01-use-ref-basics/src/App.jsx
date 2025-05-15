143970115c0fd79307eaa695ef61a61e4aed3410
explorer436 <explorer436@tutanota.com>
2025-05-14 22:53:56 -0400

    changes from explorer436-p50-20eqs27p03 on Wed May 14 10:53:56 PM EDT 2025

---
+++ front-end-development-playground/react-playground/08-UseRef/01-use-ref-basics/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import UseRefBasics from "./UseRefBasics.jsx";
+
+function App() {
+
+    return (
+        <>
+            <div className='container'>
+                <UseRefBasics/>
+            </div>
+        </>
+    )
+}
+
+export default App
