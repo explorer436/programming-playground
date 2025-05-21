021427e78bfabc339e395b9c3fc2a5e1ccb3918d
explorer436 <explorer436@tutanota.com>
2025-05-21 11:15:33 -0400

    changes from explorer436-p50-20eqs27p03 on Wed May 21 11:15:33 AM EDT 2025

---
+++ front-end-development-playground/react-playground/09-CustomHooks/01-custom-hooks-basics/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ToggleExample from "./ToggleExample.jsx";
+
+function App() {
+
+    return (
+        <>
+            <div className='container'>
+                <ToggleExample/>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ front-end-development-playground/react-playground/09-CustomHooks/03-custom-hooks-fetch-data/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import FetchData from "./FetchData.jsx";
+
+function App() {
+
+    return (
+        <>
+            <div className='container'>
+                <FetchData/>
+            </div>
+        </>
+    )
+}
+
+export default App
