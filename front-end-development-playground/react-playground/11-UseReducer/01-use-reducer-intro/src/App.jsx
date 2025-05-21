6221805f8b68729201bb1a8063c53b9189dbbb2d
explorer436 <explorer436@tutanota.com>
2025-05-21 19:04:41 -0400

    changes from explorer436-p50-20eqs27p03 on Wed May 21 07:04:41 PM EDT 2025

---
+++ front-end-development-playground/react-playground/11-UseReducer/01-use-reducer-intro/src/App.jsx
@@ -0,0 +1,11 @@
+import ReducerBasics from "./ReducerBasics.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <ReducerBasics />
+    </div>
+  );
+}
+
+export default App;
