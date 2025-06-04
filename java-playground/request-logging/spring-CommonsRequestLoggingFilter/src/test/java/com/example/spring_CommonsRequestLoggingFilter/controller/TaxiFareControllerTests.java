966ba3460eb39c4cce1ba67790acf0a3c2f428b8
explorer436 <explorer436@tutanota.com>
2025-06-04 12:04:39 -0400

    changes from explorer436-p50-20eqs27p03 on Wed Jun 4 12:04:39 PM EDT 2025

--- java-playground/request-logging/spring-CommonsRequestLoggingFilter/src/test/java/com/example/spring_CommonsRequestLoggingFilter/controller/TaxiFareControllerTests.java
+++ java-playground/request-logging/spring-CommonsRequestLoggingFilter/src/test/java/com/example/spring_CommonsRequestLoggingFilter/controller/TaxiFareControllerTests.java
@@ -2,8 +2,13 @@ package com.example.spring_CommonsRequestLoggingFilter.controller;
 
 import com.example.spring_CommonsRequestLoggingFilter.SpringCommonsRequestLoggingFilterApplication;
 import com.example.spring_CommonsRequestLoggingFilter.model.TaxiRide;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -18,13 +23,21 @@ public class TaxiFareControllerTests {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaxiFareControllerTests.class);
+
     @Test
-    public void givenRequest_whenFetchTaxiFareRateCard_thanOK() {
+    public void givenRequest_whenFetchTaxiFareRateCard_thanOK() throws JsonProcessingException {
 
         String URL = "http://localhost:" + port + "/taxifare";
 
         TestRestTemplate testRestTemplate = new TestRestTemplate();
         TaxiRide taxiRide = new TaxiRide(true, 10l);
+
+        LOGGER.info("taxiRide: {}", objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(taxiRide));
+
         String fare = testRestTemplate.postForObject(
                 URL + "/calculate/",
                 taxiRide, String.class);
