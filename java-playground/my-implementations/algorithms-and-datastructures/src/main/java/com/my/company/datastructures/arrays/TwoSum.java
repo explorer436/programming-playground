804b7a4ff40447765927a5127518195032fea65f
explorer436 <explorer436@tutanota.com>
2025-07-17 23:26:25 -0400

    changes from explorer436-Thinkpad-P50 on Thu Jul 17 11:26:25 PM EDT 2025

--- java-playground/my-implementations/algorithms-and-datastructures/src/main/java/com/my/company/datastructures/arrays/TwoSum.java
+++ java-playground/my-implementations/algorithms-and-datastructures/src/main/java/com/my/company/datastructures/arrays/TwoSum.java
@@ -4,41 +4,42 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class TwoSum {
 
-  public ImmutablePair twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
+    public ImmutablePair twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-    for (int i = 0; i < nums.length; i++) {
-      int complement = target - nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
 
-      if (map.containsKey(complement) && map.get(complement) != i) {
-        return new ImmutablePair(map.get(complement), i);
-      } else {
-        map.put(nums[i], i);
-      }
-    }
-    return null;
-  }
-
-  /**
-   * The difference between twoSum and twoSum2 is that, with twoSum2, we are running through all the
-   * elements of the input array all the time. And that may not be necessary.
-   */
-  public int[] twoSum2(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-
-    for (int i = 0; i < nums.length; i++) {
-      map.put(nums[i], i);
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new ImmutablePair(map.get(complement), i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
     }
 
-    for (int i = 0; i < nums.length; i++) {
-      int complement = target - nums[i];
-      if (map.containsKey(complement) && map.get(complement) != i) {
-        return new int[] {i, map.get(complement)};
-      }
-    }
+    /**
+     * The difference between twoSum and twoSum2 is that, with twoSum2, we are running through all the
+     * elements of the input array all the time. And that may not be necessary.
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-    return null;
-  }
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+
+        return null;
+    }
 }
