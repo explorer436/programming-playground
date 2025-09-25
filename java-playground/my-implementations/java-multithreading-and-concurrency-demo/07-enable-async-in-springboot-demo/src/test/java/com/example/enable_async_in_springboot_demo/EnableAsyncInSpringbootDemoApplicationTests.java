02e4af77fa8d3de8c3457887e4d1f6106f4980bc
explorer436 <explorer436@tutanota.com>
2025-09-24 22:53:27 -0400

    changes from parrot on Wed Sep 24 10:53:27 PM EDT 2025

---
+++ java-playground/my-implementations/java-multithreading-and-concurrency-demo/07-enable-async-in-springboot-demo/src/test/java/com/example/enable_async_in_springboot_demo/EnableAsyncInSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enable_async_in_springboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnableAsyncInSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
