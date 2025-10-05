f49d4c6371781c7370a4d001bc3498cc60254b25
explorer436 <explorer436@tutanota.com>
2025-10-04 22:32:45 -0400

    changes from explorer436-precision5540 on Sat Oct 4 10:32:45 PM EDT 2025

--- java-playground/my-implementations/algorithms-and-datastructures/src/main/java/com/my/company/datastructures/heap/Heap.java
+++ java-playground/my-implementations/algorithms-and-datastructures/src/main/java/com/my/company/datastructures/heap/Heap.java
@@ -14,7 +14,7 @@ public class Heap {
         return size == heap.length;
     }
 
-    public int getParent(int index) {
+    public int getParentNode(int index) {
         return (index - 1) / 2;
     }
 
@@ -30,18 +30,58 @@ public class Heap {
         size++;
     }
 
+    private void fixHeapAbove(int currentIndex) {
+        int newValue = heap[currentIndex];
+
+        while (currentIndex > 0 && newValue > heap[getParentNode(currentIndex)]) {
+            heap[currentIndex] = heap[getParentNode(currentIndex)];
+            currentIndex = getParentNode(currentIndex);
+        }
+
+        heap[currentIndex] = newValue;
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+
+            if (leftChild <= lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = heap[leftChild] > heap[rightChild] ? leftChild : rightChild;
+                }
+
+                if (heap[index] < heap[childToSwap]) {
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+                } else {
+                    break;
+                }
+
+                index = childToSwap;
+            } else {
+                break;
+            }
+        }
+    }
+
     public int delete(int index) {
         if (isEmpty()) {
             throw new IndexOutOfBoundsException("Heap is empty");
         }
 
-        int parent = getParent(index);
+        int parentNode = getParentNode(index);
         int deletedValue = heap[index];
 
         // grab the right most value in the heap and put it in the deleted place.
         heap[index] = heap[size - 1];
 
-        if (index == 0 || heap[index] < heap[parent]) {
+        if (index == 0 || heap[index] < heap[parentNode]) {
             fixHeapBelow(index, size - 1);
         } else {
             fixHeapAbove(index);
@@ -71,46 +111,6 @@ public class Heap {
             heap[lastHeapIndex - i] = tmp;
 
             fixHeapBelow(0, lastHeapIndex - i - 1);
-        }
-    }
-
-    private void fixHeapAbove(int index) {
-        int newValue = heap[index];
-
-        while (index > 0 && newValue > heap[getParent(index)]) {
-            heap[index] = heap[getParent(index)];
-            index = getParent(index);
-        }
-
-        heap[index] = newValue;
-    }
-
-    private void fixHeapBelow(int index, int lastHeapIndex) {
-        int childToSwap;
-
-        while (index <= lastHeapIndex) {
-            int leftChild = getChild(index, true);
-            int rightChild = getChild(index, false);
-
-            if (leftChild <= lastHeapIndex) {
-                if (rightChild > lastHeapIndex) {
-                    childToSwap = leftChild;
-                } else {
-                    childToSwap = heap[leftChild] > heap[rightChild] ? leftChild : rightChild;
-                }
-
-                if (heap[index] < heap[childToSwap]) {
-                    int temp = heap[index];
-                    heap[index] = heap[childToSwap];
-                    heap[childToSwap] = temp;
-                } else {
-                    break;
-                }
-
-                index = childToSwap;
-            } else {
-                break;
-            }
         }
     }
 
