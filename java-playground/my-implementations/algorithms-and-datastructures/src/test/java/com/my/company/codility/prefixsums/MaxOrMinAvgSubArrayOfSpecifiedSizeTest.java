b57b584246bc21aae55568d225db8077d3bf80ec
explorer436 <explorer436@tutanota.com>
2026-02-03 10:42:33 -0500

    changes from explorer436-p50-20eqs27p03 on Tue Feb 3 10:42:33 AM EST 2026

---
+++ java-playground/my-implementations/algorithms-and-datastructures/src/test/java/com/my/company/codility/prefixsums/MaxOrMinAvgSubArrayOfSpecifiedSizeTest.java
@@ -0,0 +1,16 @@
+package com.my.company.codility.prefixsums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxOrMinAvgSubArrayOfSpecifiedSizeTest {
+
+    MaxOrMinAvgSubArrayOfSpecifiedSize maxOrMinAvgSubArrayOfSpecifiedSize = new MaxOrMinAvgSubArrayOfSpecifiedSize();
+
+    @Test
+    void minAvgOfGivenSize() {
+    }
+
+
+}
