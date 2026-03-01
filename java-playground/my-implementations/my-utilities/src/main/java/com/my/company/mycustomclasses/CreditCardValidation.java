c2fb2ac842b227796338ba3df9ca5c1d2d301dfa
explorer436 <explorer436@tutanota.com>
2026-02-28 21:24:55 -0500

    changes from explorer436-precision5540 on Sat Feb 28 09:24:55 PM EST 2026

--- java-playground/my-implementations/my-utilities/src/main/java/com/my/company/mycustomclasses/CreditCardValidation.java
+++ java-playground/my-implementations/my-utilities/src/main/java/com/my/company/mycustomclasses/CreditCardValidation.java
@@ -5,11 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/*
- *
- *
- *
- */
 public class CreditCardValidation {
 
   public static void main(String args[]) throws Exception {
